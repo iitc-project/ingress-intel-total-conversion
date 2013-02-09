db5747fb73798fbdd1a177c97e14f1bf8218e7f7
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2013-02-09 15:06:48 +0100

    only pull in data from GitHub pages to reduce risk of being down.

--- code/boot.js
+++ code/boot.js
@@ -192,10 +192,10 @@ function asyncLoadScript(a){return function(b,c){var d=document.createElement("s
 
 // modified version of https://github.com/shramov/leaflet-plugins. Also
 // contains the default Ingress map style.
-var LLGMAPS = 'http://breunigs.github.com/ingress-intel-total-conversion/leaflet_google.js';
+var LLGMAPS = 'http://breunigs.github.com/ingress-intel-total-conversion/external/leaflet_google.js';
 var JQUERY = 'https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js';
 var LEAFLET = 'http://cdn.leafletjs.com/leaflet-0.5/leaflet.js';
-var AUTOLINK = 'http://raw.github.com/bryanwoods/autolink-js/master/autolink.js';
+var AUTOLINK = 'http://breunigs.github.com/ingress-intel-total-conversion/external/autolink.js';
 
 // after all scripts have loaded, boot the actual app
 load(JQUERY, LEAFLET, AUTOLINK).then(LLGMAPS).thenRun(boot);
