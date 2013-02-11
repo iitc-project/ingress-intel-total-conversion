b9a4f05adb25668bd678a74324e7b16b4b230915
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2013-02-11 19:27:16 +0100

    ignore new build file

--- autobuild.sh
+++ autobuild.sh
@@ -2,7 +2,7 @@
 
 ./build.py
 FORMAT=$(echo "\033[1;33m%w%f\033[0m written")
-while inotifywait -qre close_write --exclude "total-conversion-build.user.js|.git*" --format "$FORMAT" .
+while inotifywait -qre close_write --exclude "iitc-debug.user.js|.git*" --format "$FORMAT" .
 do
   ./build.py
 done
