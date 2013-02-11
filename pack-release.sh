5b07537ccab95127a77cf4d501a5184e520295e2
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2013-02-11 18:07:33 +0100

    debug build should not use dist/style.css

--- pack-release.sh
+++ pack-release.sh
@@ -5,4 +5,5 @@ cp ttic-debug.user.js          dist/total-conversion-build.user.js
 cp style.css                   dist/style.css
 cp external/leaflet_google.js  dist/leaflet_google.js
 cp external/autolink.js        dist/autolink.js
-echo 'Done'
+
+echo 'Change path of style.css to dist/style.css'
