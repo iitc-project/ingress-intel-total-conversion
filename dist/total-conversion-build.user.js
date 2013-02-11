e778dc8800f95c9c0c2a2ada689fa3475164db0b
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2013-02-11 18:20:01 +0100

    also apply for 0.5 users

--- dist/total-conversion-build.user.js
+++ dist/total-conversion-build.user.js
@@ -1065,7 +1065,8 @@ window.setupLargeImagePreview = function() {
 
 window.setupStyles = function() {
   $('head').append('<style>' +
-    [ '#largepreview.enl img { border:2px solid '+COLORS[TEAM_ENL]+'; } ',
+    [ 'body:after { display: none }',
+      '#largepreview.enl img { border:2px solid '+COLORS[TEAM_ENL]+'; } ',
       '#largepreview.res img { border:2px solid '+COLORS[TEAM_RES]+'; } ',
       '#largepreview.none img { border:2px solid '+COLORS[TEAM_NONE]+'; } ',
       '#chatcontrols { bottom: '+(CHAT_SHRINKED+24)+'px; }',
