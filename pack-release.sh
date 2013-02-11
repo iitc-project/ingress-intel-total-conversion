90e35ab7ddac075dc6119820530d6c7ce875d207
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2013-02-11 19:34:35 +0100

    fix typo here as well

--- pack-release.sh
+++ pack-release.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ./build.py
-cp ttic-debug.user.js          dist/total-conversion-build.user.js
+cp iitc-debug.user.js          dist/total-conversion-build.user.js
 cp style.css                   dist/style.css
 cp external/leaflet_google.js  dist/leaflet_google.js
 cp external/autolink.js        dist/autolink.js
