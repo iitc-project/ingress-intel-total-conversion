6a595b1a48c903d2df79a2a610039d72fd18e21a
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2013-02-14 10:45:05 +0100

    copy bug in pack release

--- pack-release.sh
+++ pack-release.sh
@@ -4,6 +4,7 @@
 cp iitc-debug.user.js          dist/total-conversion-build.user.js
 cp style.css                   dist/style.css
 cp external/*                  dist/
-cp -r images/                  dist/images
+rm -r dist/images/
+cp -r images/                  dist/images/
 
 echo 'Change path of style.css to dist/style.css'
