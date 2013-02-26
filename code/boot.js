d5199572836efe35d4b743aa0710c1ab5056989d
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2013-02-26 22:26:25 +0100

    use GitHub pages path again, let’s see if their rate limit and caching behaviour is better

--- code/boot.js
+++ code/boot.js
@@ -246,7 +246,7 @@ function boot() {
   window.runOnSmartphonesBeforeBoot();
 
   // overwrite default Leaflet Marker icon to be a neutral color
-  var base = 'https://raw.github.com/breunigs/ingress-intel-total-conversion/gh-pages/dist/images/';
+  var base = 'http://breunigs.github.com/ingress-intel-total-conversion/dist/images/';
   L.Icon.Default.imagePath = base;
 
   window.iconEnl = L.Icon.Default.extend({options: { iconUrl: base + 'marker-green.png' } });
