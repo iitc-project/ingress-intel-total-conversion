087964847e534aebca14096acb2c8f9cc7112816
boombuler <boombuler@googlemail.com>
2013-03-01 22:12:04 +0100

    moved nickname css class commit to different branch.
    This reverts commit 341b3d4a7d1191c0c99079c803569c83cf6a0eee.

--- code/portal_detail_display_tools.js
+++ code/portal_detail_display_tools.js
@@ -132,7 +132,6 @@ window.renderResonatorDetails = function(slot, level, nrg, dist, nick) {
 
     var meter = '<span class="meter" title="'+inf+'">' + fill + lbar + '</span>';
   }
-  nick = '<span class="nickname">'+nick+'</span>';
   return [meter, nick || ''];
 }
 
