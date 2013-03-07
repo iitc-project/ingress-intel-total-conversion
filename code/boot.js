26c18a7ddf3edf48e636735e8c309b53fc7c729d
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2013-03-07 10:49:13 +0100

    fix shadow handling in marker icons

--- code/boot.js
+++ code/boot.js
@@ -276,11 +276,11 @@ function boot() {
   window.runOnSmartphonesBeforeBoot();
 
   // overwrite default Leaflet Marker icon to be a neutral color
-  var base = 'https://iitcserv.appspot.com/dist/images/';
+  var base = 'https://iitcserv.appspot.com/dist/images';
   L.Icon.Default.imagePath = base;
 
-  window.iconEnl = L.Icon.Default.extend({options: { iconUrl: base + 'marker-green.png' } });
-  window.iconRes = L.Icon.Default.extend({options: { iconUrl: base + 'marker-blue.png' } });
+  window.iconEnl = L.Icon.Default.extend({options: { iconUrl: base + '/marker-green.png' } });
+  window.iconRes = L.Icon.Default.extend({options: { iconUrl: base + '/marker-blue.png' } });
 
   window.setupStyles();
   window.setupDialogs();
