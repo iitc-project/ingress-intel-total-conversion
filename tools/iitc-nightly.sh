08486a144e0b78cda888a900225c4640524459b1
Stefan Breunig <stefan@mathphys.fsk.uni-heidelberg.de>
2013-03-10 17:40:49 +0100

    update build script

--- tools/iitc-nightly.sh
+++ tools/iitc-nightly.sh
@@ -12,19 +12,22 @@ wget -q -O - $URL | (cd /tmp && tar xfz -)
 cd $PTH
 ./build.py
 
-date=$(date +"%Y-%m-%d")
+
+
+date=$(date +"%Y-%m-%d-%H%M%S")
 commit=$(git ls-remote --heads $GIT | head -c 10)
-# https://github.com/andreafabrizi/Dropbox-Uploader
 /opt/dropbox-up upload iitc-debug.user.js iitc-nightly/iitc-nightly-$date-$commit.user.js
 /opt/dropbox-up upload iitc-debug.user.js iitc-nightly/iitc-nightly-latest.user.js
 
-cp iitc-debug.user.js /home/pi/iitc-appengine/war/iitc-nightly/iitc-nightly-$date-$commit.user.js
-cp iitc-debug.user.js /home/pi/iitc-appengine/war/iitc-nightly/iitc-nightly-latest.user.js
+uglifyjs iitc-debug.user.js > smaller.js
+cp smaller.js /home/pi/iitc-appengine/war/iitc-nightly/iitc-nightly-$date-$commit.user.js
+cp smaller.js /home/pi/iitc-appengine/war/iitc-nightly/iitc-nightly-latest.user.js
 mv screenshots/*  /home/pi/iitc-appengine/war/screenshots
 mv images/*  /home/pi/iitc-appengine/war/images
 mv dist/images/* /home/pi/iitc-appengine/war/dist/images/
 rmdir dist/images
 mv dist/* /home/pi/iitc-appengine/war/dist/
+rsync -r iitcmbuild:/home/xeen/iitc_mobile_nightly/ /home/pi/iitc-appengine/war/mobile/
 cd /home/pi/iitc-appengine/
 rm **/*@* # not supported by app engine
 ./appenginesmall/bin/appcfg.sh update war
