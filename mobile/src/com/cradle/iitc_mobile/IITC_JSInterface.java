ce6e7561e5b0f1eefb6c05e63c56549a73972d33
leCradle <trashzopf@gmail.com>
2013-03-12 00:41:20 +0100

    added copy text to JSInterface

--- mobile/src/com/cradle/iitc_mobile/IITC_JSInterface.java
+++ mobile/src/com/cradle/iitc_mobile/IITC_JSInterface.java
@@ -1,9 +1,12 @@
 package com.cradle.iitc_mobile;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.webkit.JavascriptInterface;
+import android.widget.Toast;
 
 // provide communication between IITC script and android app
 public class IITC_JSInterface {
@@ -21,5 +24,14 @@ public class IITC_JSInterface {
 		Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
 				Uri.parse(s));
 		context.startActivity(intent);
+	}
+
+	// copy link to specific portal to android clipboard
+	@JavascriptInterface
+	public void copy(String s) {
+		ClipboardManager clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+		ClipData clip = ClipData.newPlainText("Copied Text ", s);
+			clipboard.setPrimaryClip(clip);
+		Toast.makeText(context, "copied to clipboard", Toast.LENGTH_SHORT).show();
 	}
 }
