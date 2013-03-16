8c40a72e27594915b0ab60afee9474d49bb1bad5
Jon Atkins <github@jonatkins.com>
2013-03-16 21:03:15 +0000

    fix some more github URLs

--- plugins/max-links.user.js
+++ plugins/max-links.user.js
@@ -32,7 +32,7 @@ window.plugin.maxLinks.STROKE_STYLE = {
   clickable: false,
   smoothFactor: 10
 };
-window.plugin.maxLinks._delaunayScriptLocation = 'https://raw.github.com/breunigs/ingress-intel-total-conversion/gh-pages/dist/delaunay.js';
+window.plugin.maxLinks._delaunayScriptLocation = 'http://iitc.jonatkins.com/dist/delaunay.js';
 
 window.plugin.maxLinks.layer = null;
 
