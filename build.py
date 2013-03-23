Fragger/ingress-intel-total-conversion-old

Add in base64 image encoding to build system and move images over to use it

build.py
@@ -4,6 +4,7 @@ import glob
 import time
 import re
 import io
+import base64
 
 def readfile(fn):
     with io.open(fn, 'Ur', encoding='utf8') as f:
@@ -17,6 +18,10 @@ def loaderRaw(var):
     fn = var.group(1)
     return readfile(fn)
 
+def loaderImage(var):
+    fn = var.group(1)
+    return 'data:image/png;base64,{0}'.format(str(base64.encodestring(open(fn, 'rb').read())).replace('\n', ''))
+
 
 c = '\n\n'.join(map(readfile, glob.glob('code/*')))
 n = time.strftime('%Y-%m-%d-%H%M%S')
@@ -29,6 +34,7 @@ m = '\n\n'.join(m)
 m = m.replace('@@BUILDDATE@@', n)
 m = re.sub('@@INCLUDERAW:([0-9a-zA-Z_./-]+)@@', loaderRaw, m)
 m = re.sub('@@INCLUDESTRING:([0-9a-zA-Z_./-]+)@@', loaderString, m)
+m = re.sub('@@INCLUDEIMAGE:([0-9a-zA-Z_./-]+)@@', loaderImage, m)
 
 with io.open('iitc-debug.user.js', 'w', encoding='utf8') as f:
     f.write(m)

iitc-project/ingress-intel-total-conversion

build.py
@@ -4,6 +4,7 @@ import glob
 import time
 import re
 import io
+import base64
 import sys
 import os
 import shutil
@@ -65,6 +66,10 @@ def loaderRaw(var):
     fn = var.group(1)
     return readfile(fn)
 
+def loaderImage(var):
+    fn = var.group(1)
+    return 'data:image/png;base64,{0}'.format(str(base64.encodestring(open(fn, 'rb').read())).replace('\n', ''))
+
 def loadCode(ignore):
     return '\n\n'.join(map(readfile, glob.glob('code/*')))
 
@@ -81,6 +86,7 @@ def doReplacements(script,updateUrl,downloadUrl):
 
     script = re.sub('@@INCLUDERAW:([0-9a-zA-Z_./-]+)@@', loaderRaw, script)
     script = re.sub('@@INCLUDESTRING:([0-9a-zA-Z_./-]+)@@', loaderString, script)
+    script = re.sub('@@INCLUDEIMAGE:([0-9a-zA-Z_./-]+)@@', loaderImage, script)
 
     script = script.replace('@@BUILDDATE@@', buildDate)
     script = script.replace('@@DATETIMEVERSION@@', dateTimeVersion)