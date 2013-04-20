faf3f97b0a64cd753b4e117ad0191c45d7730b50
Stephen Beitzel <sbeitzel@pobox.com>
2013-04-19 21:36:07 -0700

    Portal links in the list now zoom to the portal in the current map view without reloading the whole page.

--- plugins/list-targets.user.js
+++ plugins/list-targets.user.js
@@ -1,13 +1,24 @@
 // ==UserScript==
 // @id             list-targets@sbeitzel
 // @name           IITC plugin: list targets
-// @version        0.2.10
+// @version        0.3.06
 // @updateURL      https://raw.github.com/sbeitzel/ingress-intel-total-conversion/gh-pages/plugins/list-targets.user.js
 // @downloadURL    https://raw.github.com/sbeitzel/ingress-intel-total-conversion/gh-pages/plugins/list-targets.user.js
-// @description    Calls out links to portals which are good candidates for attack by the current user.
+// @description    Calls out links to portals which are easy candidates for attack by the current user.
 // @include        https://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
 // ==/UserScript==
+
+/**
+ * I was motivated to create this script because I live in an area with few portals, a high-level opposition, and an
+ * uncoordinated faction. As a low-level agent I struggled to find portals I could attack. As a family man with his head
+ * on straight, I had a hard time meeting up with other agents for team play. I wrote this plugin to help me find the
+ * weakest portal on the map. If you're playing with others, if your faction actually has some coordination and you can
+ * make it out to play with other people, then this plugin is probably useless to you. Multiple agents can take down
+ * any portal, and once you've destroyed and rebuilt several portals, you'll be high enough level that, again, this
+ * information will not be useful to you. However, if you're not able to play with a team, for whatever reason, and you're
+ * trying to find the best use of your bursters, this plugin might make that task a little more straightforward.
+ */
 
 function wrapper() {
   if ( !Array.prototype.forEach ) {
@@ -40,7 +51,8 @@ function wrapper() {
       // portal link
       var plink = "https://www.ingress.com/intel?latE6="+latE6+"&lngE6="+lngE6+"&pguid="+pguid;
       if (window._targetsSeen.indexOf(pguid) === -1) {
-        var dataStruct = {difficulty:0, plink:plink, title:d.portalV2.descriptiveText.TITLE};
+        var dataStruct = {difficulty:0, plink:plink, title:d.portalV2.descriptiveText.TITLE,
+                          address:d.portalV2.descriptiveText.ADDRESS, latE6:latE6, lngE6:lngE6, pguid:pguid };
         window._targetStructs.push(dataStruct);
         window._targetsSeen.push(pguid);
         // we really only need to work out the susceptibility of enemy portals
@@ -52,6 +64,86 @@ function wrapper() {
       }
     });
   };
+
+    // portal link - single click: select portal
+    //               double click: zoom to and select portal
+    //               hover: show address
+    // code from getPortalLink function by xelio from iitc: AP List - https://raw.github.com/breunigs/ingress-intel-total-conversion/gh-pages/plugins/ap-list.user.js
+    window.plugin.listTargets.getPortalLink = function(datastruct) {
+        var latlng = [datastruct.latE6/1E6, datastruct.lngE6/1E6].join();
+        var jsSingleClick = 'window.plugin.listTargets.selectPortal(\''+datastruct.pguid+'\');return false';
+        var jsDoubleClick = 'window.zoomToAndShowPortal(\''+datastruct.pguid+'\', ['+latlng+']);return false';
+        var perma = '/intel?latE6='+datastruct.latE6+'&lngE6='+datastruct.lngE6+'&z=17&pguid='+datastruct.pguid;
+
+        var atag = document.createElement("a");
+        atag.setAttribute('class', "help");
+        atag.setAttribute("title", datastruct.address);
+        atag.setAttribute("href", perma);
+        atag.setAttribute("onclick", jsSingleClick);
+        atag.setAttribute("ondblclick", jsDoubleClick);
+        atag.appendChild(document.createTextNode(datastruct.title));
+
+        var div = document.createElement("div");
+        div.setAttribute("style", "max-height: 15px !important; min-width:140px !important;max-width:180px !important; overflow: hidden; text-overflow:ellipsis;");
+        div.appendChild(atag);
+
+        return div;
+    };
+
+    // selectPortal - copied from ap-list plugin
+    window.plugin.listTargets.selectPortal = function(guid) {
+      // Add error catching to avoid following link of portal if error
+      // occured in renderPortalDetails or hooked plugin
+      try {
+        renderPortalDetails(guid);
+      } catch(e) {
+        console.error(e.message);
+        console.log(e.stack);
+        console.log('Skipping error in renderPortalDetails or hooked plugin')
+      }
+      window.plugin.listTargets.setPortalLocationIndicator(guid);
+    };
+
+    // setPortalLocationIndicator - copied from ap-list plugin
+    window.plugin.listTargets.setPortalLocationIndicator = function(guid) {
+      var portal = window.portals[guid];
+      if(!portal) return;
+      var startRadius = screen.availWidth / 2;
+      var portalRadius = portal.options.radius;
+      var latlng = portal.getLatLng();
+      var property = {
+        radius: startRadius,
+        fill: false,
+        color: COLOR_SELECTED_PORTAL,
+        weight: 2,
+        opacity: 1,
+        portalRadius: portalRadius,
+        clickable: false };
+
+      if(plugin.listTargets.portalLocationIndicator)
+        map.removeLayer(plugin.listTargets.portalLocationIndicator);
+      if(plugin.listTargets.animTimeout)
+        clearTimeout(plugin.apList.animTimeout);
+      plugin.listTargets.portalLocationIndicator = L.circleMarker(latlng, property).addTo(map);
+      plugin.listTargets.animTimeout = setTimeout(plugin.listTargets.animPortalLocationIndicator,100);
+    };
+
+    // animPortalLocationIndicator - copied from ap-list plugin
+    window.plugin.listTargets.animPortalLocationIndicator = function() {
+      var radius = plugin.listTargets.portalLocationIndicator.options.radius;
+      var portalRadius = plugin.listTargets.portalLocationIndicator.options.portalRadius;
+      if(radius > portalRadius) {
+        var step = radius / 3;
+        if(radius < 80) step = step / 3;
+        var newRadius = plugin.listTargets.portalLocationIndicator.options.radius -= step;
+        plugin.listTargets.portalLocationIndicator.setRadius(newRadius);
+        if(plugin.listTargets.animTimeout)
+          clearTimeout(plugin.listTargets.animTimeout);
+        plugin.listTargets.animTimeout = setTimeout(plugin.listTargets.animPortalLocationIndicator,100);
+      } else {
+        map.removeLayer(plugin.listTargets.portalLocationIndicator);
+      }
+    };
 
   var setup =  function() {
     // add the UI
@@ -105,17 +197,19 @@ function wrapper() {
     var tbody = document.createElement("tbody");
     for (var i =0; i < window._targetStructs.length; i++) {
       var datastruct = window._targetStructs[i];
-      console.log("add row for datastruct");
-      console.log(datastruct);
       var row = document.createElement("tr");
       var cell = document.createElement("td");
       cell.appendChild(document.createTextNode(datastruct.difficulty));
       row.appendChild(cell);
       cell = document.createElement("td");
-      var anchor = document.createElement("a");
-      anchor.appendChild(document.createTextNode(datastruct.title));
-      anchor.setAttribute("href", datastruct.plink);
+      var anchor = window.plugin.listTargets.getPortalLink(datastruct);
+        // debugging
+        console.log("anchor:");
+        console.log(anchor);
+
       cell.appendChild(anchor);
+        console.log("cell");
+        console.log(cell);
       row.appendChild(cell);
       tbody.appendChild(row);
     };
