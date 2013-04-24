4a16668c6ac49154a5c5a109cee374272f913e6d
Jon Atkins <github@jonatkins.com>
2013-04-24 02:40:53 +0100

    change player tracker display for unknown plyaer levels, to match that used in the guess player levels plugin

--- plugins/player-tracker.user.js
+++ plugins/player-tracker.user.js
@@ -266,12 +266,13 @@ window.plugin.playerTracker.drawData = function() {
     if(window.plugin.guessPlayerLevels !== undefined &&
        window.plugin.guessPlayerLevels.fetchLevelByPlayer !== undefined) {
       var playerLevel = window.plugin.guessPlayerLevels.fetchLevelByPlayer(pguid);
-      if (playerLevel === undefined) playerLevel = 1;  //if player level unknown, assume level 1
       if(playerLevel !== undefined) {
         title += '<span style="font-weight:bold;margin-left:10px;">Level '
           + playerLevel
           + (playerLevel < (window.MAX_XM_PER_LEVEL.length - 1) ? ' (guessed)' : '')
           + '</span>';
+      } else {
+        title += '<span style="font-weight:bold;margin-left:10px;">Level unknown</span>'
       }
     }
     
