b98cf6b7ae9701e4834fc6d464396d4047f2aedb
Jon Atkins <github@jonatkins.com>
2013-04-30 19:04:01 +0100

    website - tracking mobile shouldn't always point to 'release' - use apk relative path

--- website/page/code/mobile-download.php
+++ website/page/code/mobile-download.php
@@ -54,7 +54,7 @@ IITC Mobile version <?php print $apk_version; ?>, with IITC version <?php print 
 </p>
 
 <p>
-<a style="margin-right: 1em;" onclick="if(track)({track{'mobile','download','release');}" class="btn btn-large btn-primary" href="<?php print $apkfile; ?>">Download</a> or scan the QR Code
+<a style="margin-right: 1em;" onclick="if(track)({track{'mobile','download','$apkfile');}" class="btn btn-large btn-primary" href="<?php print $apkfile; ?>">Download</a> or scan the QR Code
 </p>
 
 </div>
