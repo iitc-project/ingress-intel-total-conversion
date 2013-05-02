039167051ede41297d40ff2172c4ed97458e8fc7
Jon Atkins <github@jonatkins.com>
2013-05-02 04:19:31 +0100

    website - new IITC release

--- website/page/home.php
+++ website/page/home.php
@@ -13,15 +13,22 @@ offers many more features. It is available for
 
 <h3>Latest news</h3>
 
+<h4>2nd May 2013</h4>
+<p>
+IITC version 0.11.3 has been released. This should vastly reduce the chance of getting REQUEST_FAILED errors
+while scrolling/zooming the map frequently. The passcode redemption code has been updated, and there are improvements
+to the URL link handling. The portals-list plugin has been updated, and bug fixes made to the
+player tracker. A new plugin to show portal levels as numbers has been added too.
+</p>
+<p>
+IITC Mobile 0.3.2 is also available. Along with the above, this includes a new option to show your current
+position on the map.
+</p>
+
 <h4>28th April 2013</h4>
 <p>
 New website launched! A major revamp of the website has been made. Thanks to the various users who contributed
 logos and site templates.
 </p>
 
-<h4>26th April 2013</h4>
-<p>
-IITC 0.11.2 released. This has a minor fix relating to portal visibility at different zoom levels. We now have to 
-match the standard intel site. This does, unfortunately, mean you need to zoom even closer to see unclaimed portals.
-Also, an update to the scoreboard plugin has been released that should make it work again.
-</p>
+<a class="btn btn-small" href="?page=news">Older news</a>
--- website/page/news.php
+++ website/page/news.php
@@ -1,5 +1,17 @@
 <h2>News</h2>
 
+<h4>2nd May 2013</h4>
+<p>
+IITC version 0.11.3 has been released. This should vastly reduce the chance of getting REQUEST_FAILED errors
+while scrolling/zooming the map frequently. The passcode redemption code has been updated, and there are improvements
+to the URL link handling. The portals-list plugin has been updated, and bug fixes made to the
+player tracker. A new plugin to show portal levels as numbers has been added too.
+</p>
+<p>
+IITC Mobile 0.3.2 is also available. Along with the above, this includes a new option to show your current
+position on the map.
+</p>
+
 <h4>28th April 2013</h4>
 <p>
 New website launched! A major revamp of the website has been made. Thanks to the various users who contributed
