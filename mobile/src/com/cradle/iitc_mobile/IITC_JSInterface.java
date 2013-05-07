0c915b776ab1879171f00f27ae92f8b17a34938a
Kai Stempfle <kai.stempfle@gmail.com>
2013-05-08 00:38:06 +0200

    revert removal of intentPosLink()

--- mobile/src/com/cradle/iitc_mobile/IITC_JSInterface.java
+++ mobile/src/com/cradle/iitc_mobile/IITC_JSInterface.java
@@ -3,6 +3,8 @@ package com.cradle.iitc_mobile;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.webkit.JavascriptInterface;
 import android.widget.Toast;
 
@@ -25,5 +27,15 @@ public class IITC_JSInterface {
         clipboard.setPrimaryClip(clip);
         Toast.makeText(context, "copied to clipboard", Toast.LENGTH_SHORT)
                 .show();
+    }
+
+	// send geo intent for navigation apps like gmaps or waze etc...
+	@JavascriptInterface
+	public void intentPosLink(String lat, String lng, String portal_name) {
+		String uri = "geo:" + lat + "," + lng + "?q=" + lat + "," + lng
+				             + portal_name;
+		Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+				                          Uri.parse(uri));
+		context.startActivity(intent);
     }
 }
