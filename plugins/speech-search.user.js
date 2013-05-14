7d53f35714f1e5cbc8ba058b648124215420545e
Philipp Schaefer <trashzopf@gmail.com>
2013-05-14 19:43:08 +0200

    fixed indentation on speech-search plugin header to match the others

--- plugins/speech-search.user.js
+++ plugins/speech-search.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
-// @id iitc-plugin-speech-search
-// @name IITC Plugin: Speech Search
-// @version 0.0.1.@@DATETIMEVERSION@@
-// @namespace https://github.com/jonatkins/ingress-intel-total-conversion
-// @updateURL @@UPDATEURL@@
-// @downloadURL @@DOWNLOADURL@@
-// @description [@@BUILDNAME@@-@@BUILDDATE@@] Allow speech input for location search (webkit only for now - NOT Firefox)
-// @include https://www.ingress.com/intel*
-// @include http://www.ingress.com/intel*
-// @match https://www.ingress.com/intel*
-// @match http://www.ingress.com/intel*
+// @id             iitc-plugin-speech-search
+// @name           IITC Plugin: Speech Search
+// @version        0.0.1.@@DATETIMEVERSION@@
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      @@UPDATEURL@@
+// @downloadURL    @@DOWNLOADURL@@
+// @description    [@@BUILDNAME@@-@@BUILDDATE@@] Allow speech input for location search (webkit only for now - NOT Firefox)
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
 // ==/UserScript==
 
 function wrapper() {
