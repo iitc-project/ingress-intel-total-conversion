756445a77054405f896b473a36822ab001e79584
marstone <i@marstone.net>
2013-05-20 00:20:47 +0800

    change meta: build date

--- plugins/comm-blacklist.user.js
+++ plugins/comm-blacklist.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @id             iitc-plugin-comm-blacklist@marstone
 // @name           IITC plugin: blacklist
-// @version        0.1
+// @version        0.1.0.@@DATETIMEVERSION@@
 // @namespace      https://github.com/marstone/ingress-intel-total-conversion
 // @updateURL      @@UPDATEURL@@
 // @downloadURL    @@DOWNLOADURL@@
