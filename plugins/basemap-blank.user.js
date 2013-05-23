2872b2bad4eafbe9255ec4d7f0729bdaa31a9d00
Jon Atkins <github@jonatkins.com>
2013-05-23 05:44:01 +0100

    add blank map layer - for #220

---
+++ plugins/basemap-blank.user.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @id             iitc-plugin-basemap-blank@jonatkins
+// @name           IITC plugin: Blank map
+// @version        0.1.0.@@DATETIMEVERSION@@
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      @@UPDATEURL@@
+// @downloadURL    @@DOWNLOADURL@@
+// @description    [@@BUILDNAME@@-@@BUILDDATE@@] Add a blank map layer - no roads or other features.
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @grant          none
+// ==/UserScript==
+
+function wrapper() {
+// ensure plugin framework is there, even if iitc is not yet loaded
+if(typeof window.plugin !== 'function') window.plugin = function() {};
+
+
+// PLUGIN START ////////////////////////////////////////////////////////
+
+
+// use own namespace for plugin
+window.plugin.mapTileBlank = function() {};
+
+window.plugin.mapTileBlank.addLayer = function() {
+
+  var blankOpt = {attribution: '', maxZoom: 20};
+  var blank = new L.TileLayer('@@INCLUDEIMAGE:plugins/basemap-blank-tile.png@@', blankOpt);
+
+  layerChooser.addBaseLayer(blank, "Blank Map");
+};
+
+var setup =  window.plugin.mapTileBlank.addLayer;
+
+// PLUGIN END //////////////////////////////////////////////////////////
+
+if(window.iitcLoaded && typeof setup === 'function') {
+  setup();
+} else {
+  if(window.bootPlugins)
+    window.bootPlugins.push(setup);
+  else
+    window.bootPlugins = [setup];
+}
+} // wrapper end
+// inject code into site context
+var script = document.createElement('script');
+script.appendChild(document.createTextNode('('+ wrapper +')();'));
+(document.body || document.head || document.documentElement).appendChild(script);
