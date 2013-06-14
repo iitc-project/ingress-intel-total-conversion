47f4659ca0b03a3350213c9d27c972ba67ea836f
Jon Atkins <github@jonatkins.com>
2013-06-14 20:30:05 +0100

    apply the new plugin wrapper change to the mobile-only user-location plugin

--- mobile/user-location.user.js
+++ mobile/user-location.user.js
@@ -12,10 +12,7 @@
 // @match          http://www.ingress.com/intel*
 // ==/UserScript==
 
-function wrapper() {
-// ensure plugin framework is there, even if iitc is not yet loaded
-if(typeof window.plugin !== 'function') window.plugin = function() {};
-
+@@PLUGINSTART@@
 
 // PLUGIN START ////////////////////////////////////////////////////////
 
@@ -53,16 +50,4 @@ var setup = window.plugin.userLocation.setup;
 
 // PLUGIN END //////////////////////////////////////////////////////////
 
-if(window.iitcLoaded && typeof setup === 'function') {
-  setup();
-} else {
-  if(window.bootPlugins)
-    window.bootPlugins.push(setup);
-  else
-    window.bootPlugins = [setup];
-}
-} // wrapper end
-// inject code into site context
-var script = document.createElement('script');
-script.appendChild(document.createTextNode('('+ wrapper +')();'));
-(document.body || document.head || document.documentElement).appendChild(script);
+@@PLUGINEND@@
