f6e4b40c0c28197a9354d0f5607425c5e352a646
Jon Atkins <github@jonatkins.com>
2013-06-29 18:06:46 +0100

    plugin to disable map zooming with the mouse wheel fix #398

---
+++ plugins/scroll-wheel-zoom-disable.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @id             iitc-plugin-scroll-wheel-zoom-disable@jonatkins
+// @name           IITC plugin: disable mouse wheel zoom
+// @category       Tweaks
+// @version        0.1.0.@@DATETIMEVERSION@@
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      @@UPDATEURL@@
+// @downloadURL    @@DOWNLOADURL@@
+// @description    [@@BUILDNAME@@-@@BUILDDATE@@] Disable the use of mouse wheel to zoom. The map zoom controls or keyboard are still available.
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @grant          none
+// ==/UserScript==
+
+
+@@PLUGINSTART@@
+
+// PLUGIN START ////////////////////////////////////////////////////////
+
+
+// use own namespace for plugin
+window.plugin.scrollWheelZoomDisable = function() {};
+
+window.plugin.scrollWheelZoomDisable.setup = function() {
+
+  window.map.scrollWheelZoom.disable();
+
+};
+
+var setup =  window.plugin.scrollWheelZoomDisable.setup;
+
+// PLUGIN END //////////////////////////////////////////////////////////
+
+@@PLUGINEND@@
