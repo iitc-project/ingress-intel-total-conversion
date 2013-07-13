8b2814846a60a6eb36c3468c6cd535f3fababa6c
Jon Atkins <github@jonatkins.com>
2013-07-13 18:41:11 +0100

    remove description part mentioning 'revenge' - not really something we should encourage.

--- plugins/players-resonators.user.js
+++ plugins/players-resonators.user.js
@@ -5,7 +5,7 @@
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      @@UPDATEURL@@
 // @downloadURL    @@DOWNLOADURL@@
-// @description    [@@BUILDNAME@@-@@BUILDDATE@@] The plugins finds the resonators of a given player. The input is in the sidebar. Useful for revenge.
+// @description    [@@BUILDNAME@@-@@BUILDDATE@@] The plugins finds the resonators of a given player. The input is in the sidebar.
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
