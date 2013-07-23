dd2a24851181f1b6c9dd7fb565fb0b983eaf1763
Giacinto Garcea <info@giacintogarcea.com>
2013-07-23 22:34:29 +0200

    Edit 3 new plugins
    - Removed "chat-msg-length";
    - "player-range": also shows the xmp ranges;
    - "zaprange": adds a strikeout to the zaprange checkbox label when when
    zoomed out too far;

--- plugins/player-range.user.js
+++ plugins/player-range.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @id             iitc-plugin-player-range@zaso
-// @name           IITC plugin: Player Range
+// @name           IITC plugin: Player Range (hack and XMP)
 // @category       Layer
-// @version        0.1.0.@@DATETIMEVERSION@@
+// @version        0.1.3.@@DATETIMEVERSION@@
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      @@UPDATEURL@@
 // @downloadURL    @@DOWNLOADURL@@
-// @description    [@@BUILDNAME@@-@@BUILDDATE@@] Add one or more player markers and his range on the map.
+// @description    [@@BUILDNAME@@-@@BUILDDATE@@] Add one or more player markers and his ranges (hack range and xmp range) on the map.
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
@@ -21,82 +21,187 @@
   // use own namespace for plugin
   window.plugin.playerRange = function() {};
 
-  window.plugin.playerRange.pLayerGroup = new L.LayerGroup();
+  window.plugin.playerRange.locStore = 'pluginPlayerRange';
 
   window.plugin.playerRange.setupCSS = function() {
     var colorFaction;
     switch(window.PLAYER.team) {
-      case('ALIENS'): colorFaction = 'left bottom'; break;
-      case('RESISTANCE'): colorFaction = '-19px bottom'; break;
+      case('ALIENS'): colorFaction = 'left top'; break;
+      case('RESISTANCE'): colorFaction = 'right top'; break;
       default:colorFaction = 'left bottom'; break;
     }
 
-    $("<style>").prop("type", "text/css").html(''
-      +'.playerIcon,.playerRangeButton{'
-        +'background-position:'+colorFaction+';'
-        +'background-repeat:no-repeat;'
-        +'background-image:url(@@INCLUDEIMAGE:plugins/player-range.png@@);'
-      +'}'
-      +'.playerRangeButton{background-position:-36px center !important;}'
+    $("<style>").prop("type", "text/css").html(
+      '.playerIcon,.playerRangeButton,.setRangeButton{background-position:'+colorFaction+';background-repeat:no-repeat;background-image:url(@@INCLUDEIMAGE:plugins/player-range.png@@);}'
+      +'.playerRangeButton{background-position:6px -25px !important;}'
+      +'.setRangeButton{background-position:-18px -25px !important;border-radius:0 0 4px 4px;}'
+      +'.rangeList{display:none;position:absolute;background:#fff;padding:7px;left:35px;top:27px;width:70px;}'
+      +'.rangeList input{height:auto}'
     ).appendTo("head");
   };
 
-  window.plugin.playerRange.myDraw = function() {
+  window.plugin.playerRange.drawMarker = function() {
     var latlng = map.getCenter();
     var icc = new L.divIcon({
       iconSize: new L.Point(19, 25),
       className:'playerIcon',
     });
 
-    var mar = new L.Marker(latlng, {icon:icc, draggable:true});
-    var circle = new L.circle(latlng, 40, {color:"#eb0",fill:false,weight:2,clickable:false});
+    var arrowMark = new L.Marker(latlng, {icon:icc, draggable:true});
+    var circleHack = new L.circle(latlng, 40, {color:"#eb0",fill:false,weight:1,clickable:false,opacity:0.7});
+    var circleL1 = new L.circle(latlng, 42, {color:"#FECE5A",fill:false,weight:1,clickable:false,opacity:0.7});
+    var circleL2 = new L.circle(latlng, 48, {color:"#FFA630",fill:false,weight:1,clickable:false,opacity:0.7});
+    var circleL3 = new L.circle(latlng, 58, {color:"#FF7315",fill:false,weight:1,clickable:false,opacity:0.7});
+    var circleL4 = new L.circle(latlng, 72, {color:"#E40000",fill:false,weight:1,clickable:false,opacity:0.7});
+    var circleL5 = new L.circle(latlng, 90, {color:"#FD2992",fill:false,weight:1,clickable:false,opacity:0.7});
+    var circleL6 = new L.circle(latlng, 112, {color:"#EB26CD",fill:false,weight:1,clickable:false,opacity:0.7});
+    var circleL7 = new L.circle(latlng, 138, {color:"#C124E0",fill:false,weight:1,clickable:false,opacity:0.7});
+    var circleL8 = new L.circle(latlng, 168, {color:"#9627F4",fill:false,weight:1,clickable:false,opacity:0.7});
 
-    mar.addTo(window.plugin.playerRange.pLayerGroup);
-    circle.addTo(window.plugin.playerRange.pLayerGroup);
+    arrowMark.addTo(window.plugin.playerRange.mark);
+    circleHack.addTo(window.plugin.playerRange.hack);
+    circleL1.addTo(window.plugin.playerRange.L1);
+    circleL2.addTo(window.plugin.playerRange.L2);
+    circleL3.addTo(window.plugin.playerRange.L3);
+    circleL4.addTo(window.plugin.playerRange.L4);
+    circleL5.addTo(window.plugin.playerRange.L5);
+    circleL6.addTo(window.plugin.playerRange.L6);
+    circleL7.addTo(window.plugin.playerRange.L7);
+    circleL8.addTo(window.plugin.playerRange.L8);
 
-    mar.on('drag', function(e) {
-      circle.setLatLng(mar.getLatLng());
+    arrowMark.on('drag', function(e) {
+      circleHack.setLatLng(arrowMark.getLatLng());
+      circleL1.setLatLng(arrowMark.getLatLng());
+      circleL2.setLatLng(arrowMark.getLatLng());
+      circleL3.setLatLng(arrowMark.getLatLng());
+      circleL4.setLatLng(arrowMark.getLatLng());
+      circleL5.setLatLng(arrowMark.getLatLng());
+      circleL6.setLatLng(arrowMark.getLatLng());
+      circleL7.setLatLng(arrowMark.getLatLng());
+      circleL8.setLatLng(arrowMark.getLatLng());
     });
 
-    mar.on('dblclick', function(e) {
-      map.removeLayer(circle);
-      map.removeLayer(mar);
+    arrowMark.on('dblclick', function(e) {
+      map.removeLayer(circleHack);
+      map.removeLayer(circleL1);
+      map.removeLayer(circleL2);
+      map.removeLayer(circleL3);
+      map.removeLayer(circleL4);
+      map.removeLayer(circleL5);
+      map.removeLayer(circleL6);
+      map.removeLayer(circleL7);
+      map.removeLayer(circleL8);
+      map.removeLayer(arrowMark);
     });
 
-    mar.on('remove', function(e) {
-      mar.off('drag');
-      mar.off('dblclick');
-      mar.off('remove');
+    arrowMark.on('remove', function(e) {
+      arrowMark.off('drag');
+      arrowMark.off('dblclick');
+      arrowMark.off('remove');
     });
 
     map.on('zoomend', function(e) {
-      mar.setLatLng(circle.getLatLng());
+      arrowMark.setLatLng(circleHack.getLatLng());
     });
   }
 
+  window.plugin.playerRange.setRange = function(el) {
+    var range = el.data('range');
+    var ls = JSON.parse(localStorage[window.plugin.playerRange.locStore]);
+
+    if(el.is(':checked')) {
+      window.plugin.playerRange.pLayerGroup.addLayer(window.plugin.playerRange[range]);
+      ls[range] = 1;
+    }else{
+      window.plugin.playerRange.pLayerGroup.removeLayer(window.plugin.playerRange[range]);
+      ls[range] = 0;
+    }
+    localStorage[window.plugin.playerRange.locStore] = JSON.stringify(ls);
+  }
+
+  window.plugin.playerRange.setupHTML = ''
+    +'<div class="leaflet-playerrange leaflet-control">'
+      +'<div class="leaflet-bar">'
+        +'<a class="playerRangeButton" onclick="window.plugin.playerRange.drawMarker();return false;" title="Add a player marker.\n\nDoubleClick on a marker to remove it."></a>'
+        +'<a class="setRangeButton" onclick="$(\'.rangeList\').toggle();return false;" title="Click to set the ranges."></a>'
+        +'<div class="rangeList leaflet-bar">'
+          +'<label><input type="checkbox" checked name="xmpRange" data-range="hack" /> Hack</label><br/>'
+          +'<label><input type="checkbox" checked name="xmpRange" data-range="L1" /> XMP L1</label><br/>'
+          +'<label><input type="checkbox" checked name="xmpRange" data-range="L2" /> XMP L2</label><br/>'
+          +'<label><input type="checkbox" checked name="xmpRange" data-range="L3" /> XMP L3</label><br/>'
+          +'<label><input type="checkbox" checked name="xmpRange" data-range="L4" /> XMP L4</label><br/>'
+          +'<label><input type="checkbox" checked name="xmpRange" data-range="L5" /> XMP L5</label><br/>'
+          +'<label><input type="checkbox" checked name="xmpRange" data-range="L6" /> XMP L6</label><br/>'
+          +'<label><input type="checkbox" checked name="xmpRange" data-range="L7" /> XMP L7</label><br/>'
+          +'<label><input type="checkbox" checked name="xmpRange" data-range="L8" /> XMP L8</label><br/>'
+        +'</div>'
+      +'</div>'
+    +'</div>';
+
   var setup = function() {
     window.plugin.playerRange.setupCSS();
+
+    window.plugin.playerRange.pLayerGroup = new L.LayerGroup();
+
+    window.plugin.playerRange.mark = new L.LayerGroup();
+    window.plugin.playerRange.hack = new L.LayerGroup();
+    window.plugin.playerRange.L1 = new L.LayerGroup();
+    window.plugin.playerRange.L2 = new L.LayerGroup();
+    window.plugin.playerRange.L3 = new L.LayerGroup();
+    window.plugin.playerRange.L4 = new L.LayerGroup();
+    window.plugin.playerRange.L5 = new L.LayerGroup();
+    window.plugin.playerRange.L6 = new L.LayerGroup();
+    window.plugin.playerRange.L7 = new L.LayerGroup();
+    window.plugin.playerRange.L8 = new L.LayerGroup();
+
+    window.plugin.playerRange.pLayerGroup.addLayer(window.plugin.playerRange.mark);
+    window.plugin.playerRange.pLayerGroup.addLayer(window.plugin.playerRange.hack);
+    window.plugin.playerRange.pLayerGroup.addLayer(window.plugin.playerRange.L1);
+    window.plugin.playerRange.pLayerGroup.addLayer(window.plugin.playerRange.L2);
+    window.plugin.playerRange.pLayerGroup.addLayer(window.plugin.playerRange.L3);
+    window.plugin.playerRange.pLayerGroup.addLayer(window.plugin.playerRange.L4);
+    window.plugin.playerRange.pLayerGroup.addLayer(window.plugin.playerRange.L5);
+    window.plugin.playerRange.pLayerGroup.addLayer(window.plugin.playerRange.L6);
+    window.plugin.playerRange.pLayerGroup.addLayer(window.plugin.playerRange.L7);
+    window.plugin.playerRange.pLayerGroup.addLayer(window.plugin.playerRange.L8);
+
     window.addLayerGroup('Player Range', window.plugin.playerRange.pLayerGroup, true);
 
-    L.Control.Command = L.Control.extend({
-      options:{position: 'topleft'},
+    $('.leaflet-control-container .leaflet-top.leaflet-left').append(window.plugin.playerRange.setupHTML);
 
-      onAdd:function(map) {
-        var controlDiv = L.DomUtil.create('div', 'leaflet-control-player-range');
-        var controlSubDIV = L.DomUtil.create('div','leaflet-bar', controlDiv);
-        var controlButton = L.DomUtil.create('a', 'playerRangeButton', controlSubDIV);
-        controlButton.title = 'Add a Player Marker.\n\nDoubleClick on a marker to remove it.';
+    $('.rangeList input[name=xmpRange]').change(function() {
+      window.plugin.playerRange.setRange($(this));
+    });
 
-        L.DomEvent
-          .addListener(controlDiv, 'click', L.DomEvent.stopPropagation)
-          .addListener(controlDiv, 'click', L.DomEvent.preventDefault)
-          .addListener(controlDiv, 'click', function() { window.plugin.playerRange.myDraw(); });
-        return controlDiv;
+    //hide the controls when the layer is off, show it when on
+    map.on('layeradd', function(obj) {
+      if(obj.layer === window.plugin.playerRange.pLayerGroup) {
+        $('.leaflet-playerrange').show();
       }
     });
-    L.control.command = function(options) { return new L.Control.Command(options); };
+    map.on('layerremove', function(obj) {
+      if(obj.layer === window.plugin.playerRange.pLayerGroup) {
+        $('.leaflet-playerrange').hide();
+      }
+    });
+    if(!map.hasLayer(window.plugin.playerRange.pLayerGroup)) {
+      $('.leaflet-playerrange').hide();
+    }
 
-    map.addControl(new L.control.command());
+    if(!localStorage[window.plugin.playerRange.locStore]) {
+      localStorage[window.plugin.playerRange.locStore] = '{"hack":1,"L1":1,"L2":1,"L3":1,"L4":1,"L5":1,"L6":1,"L7":1,"L8":1}'
+    } else {
+      var rangeList = JSON.parse(localStorage[window.plugin.playerRange.locStore]);
+      for(var key in rangeList) {
+        if(rangeList[key] === 1) {
+          $('.rangeList input[data-range='+key+']').attr('checked', true);
+          window.plugin.playerRange.pLayerGroup.addLayer(window.plugin.playerRange[key]);
+        } else {
+          $('.rangeList input[data-range='+key+']').attr('checked', false);
+          window.plugin.playerRange.pLayerGroup.removeLayer(window.plugin.playerRange[key]);
+        }
+      }
+    }
   }
 
 
--- plugins/zaprange.user.js
+++ plugins/zaprange.user.js
@@ -2,7 +2,7 @@
 // @id             iitc-plugin-zaprange@zaso
 // @name           IITC plugin: Zaprange
 // @category       Layer
-// @version        0.1.1.@@DATETIMEVERSION@@
+// @version        0.1.2.@@DATETIMEVERSION@@
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      @@UPDATEURL@@
 // @downloadURL    @@DOWNLOADURL@@
@@ -20,16 +20,12 @@
 
   // use own namespace for plugin
   window.plugin.zaprange = function() {};
-
   window.plugin.zaprange.zapLayers = {};
-  window.plugin.zaprange.zapLayerGroup = new L.LayerGroup();
-  window.plugin.zaprange.zStart;
-  window.plugin.zaprange.zEnd;
+  window.plugin.zaprange.MIN_MAP_ZOOM = 16;
 
   window.plugin.zaprange.portalAdded = function(data) {
     data.portal.on('add', function() {
       window.plugin.zaprange.draw(this.options.guid);
-      window.plugin.zaprange.hide();
     });
 
     data.portal.on('remove', function() {
@@ -40,7 +36,7 @@
   window.plugin.zaprange.remove = function(guid) {
     var previousLayer = window.plugin.zaprange.zapLayers[guid];
     if(previousLayer) {
-      window.plugin.zaprange.zapLayerGroup.removeLayer(previousLayer);
+      window.plugin.zaprange.zapCircleHolderGroup.removeLayer(previousLayer);
       delete window.plugin.zaprange.zapLayers[guid];
     }
   }
@@ -53,43 +49,46 @@
       var coo = d._latlng;
       var latlng = new L.LatLng(coo.lat,coo.lng);
       var portalLevel = parseInt(getPortalLevel(dd));
-      var optCircle = {color:'red',fill:false,weight:2,clickable:false, dashArray: [10,6]};
+      var optCircle = {color:'red',opacity:0.7,fillColor:'red',fillOpacity:0.1,weight:1,clickable:false, dashArray: [10,6]};
       var range = (5*portalLevel)+35;
 
       var circle = new L.Circle(latlng, range, optCircle);
       window.plugin.zaprange.zapLayers[guid] = circle;
-      circle.addTo(window.plugin.zaprange.zapLayerGroup);
+      circle.addTo(window.plugin.zaprange.zapCircleHolderGroup);
     }
   }
 
-  window.plugin.zaprange.hide = function() {
-    var z1 = window.plugin.zaprange.zStart;
-    var z2 = window.plugin.zaprange.zEnd;
-
-    if((z1 > 15 && z2 <= 15) || (z1 <= 15 && z2 <= 15)) {
-      window.plugin.zaprange.zapLayerGroup.eachLayer(function(layer) {
-        layer.setStyle({opacity:0});
-      });
-    }else if(z1 <= 15 && z2 > 15) {
-      window.plugin.zaprange.zapLayerGroup.eachLayer(function(layer) {
-        layer.setStyle({opacity:0.7});
-      });
+  window.plugin.zaprange.showOrHide = function() {
+    if(map.getZoom() >= window.plugin.zaprange.MIN_MAP_ZOOM) {
+      // show the layer
+      if(!window.plugin.zaprange.zapLayerHolderGroup.hasLayer(window.plugin.zaprange.zapCircleHolderGroup)) {
+        window.plugin.zaprange.zapLayerHolderGroup.addLayer(window.plugin.zaprange.zapCircleHolderGroup);
+        $('.leaflet-control-layers-list span:contains("Zaprange")').parent().removeClass('disabled').attr('title', '');
+      }
+    } else {
+      // hide the layer
+      if(window.plugin.zaprange.zapLayerHolderGroup.hasLayer(window.plugin.zaprange.zapCircleHolderGroup)) {
+        window.plugin.zaprange.zapLayerHolderGroup.removeLayer(window.plugin.zaprange.zapCircleHolderGroup);
+        $('.leaflet-control-layers-list span:contains("Zaprange")').parent().addClass('disabled').attr('title', 'Zoom in to show those.');
+      }
     }
   }
 
-  var setup =  function() {
-    window.plugin.zaprange.zStart = window.plugin.zaprange.zEnd = map.getZoom();
+  var setup = function() {
+    // this layer is added to the layer chooser, to be toggled on/off
+    window.plugin.zaprange.zapLayerHolderGroup = new L.LayerGroup();
 
-    window.addLayerGroup('Zap range', window.plugin.zaprange.zapLayerGroup, true);
+    // this layer is added into the above layer, and removed from it when we zoom out too far
+    window.plugin.zaprange.zapCircleHolderGroup = new L.LayerGroup();
+
+    window.plugin.zaprange.zapLayerHolderGroup.addLayer(window.plugin.zaprange.zapCircleHolderGroup);
+
+    window.addLayerGroup('Zaprange', window.plugin.zaprange.zapLayerHolderGroup, true);
     window.addHook('portalAdded', window.plugin.zaprange.portalAdded);
 
-    map.on('zoomstart', function(e) {
-      window.plugin.zaprange.zStart = map.getZoom();
-    });
-    map.on('zoomend', function(e) {
-      window.plugin.zaprange.zEnd = map.getZoom();
-      window.plugin.zaprange.hide();
-    });
+    map.on('zoomend', window.plugin.zaprange.showOrHide);
+
+    window.plugin.zaprange.showOrHide();
   }
 
 // PLUGIN END //////////////////////////////////////////////////////////
