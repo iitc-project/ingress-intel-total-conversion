bcf1c44f02be70ee7dd52678920a7cd18874058f
Jan Bader <jan.bader@ub-software.de>
2013-07-25 16:37:42 +0200

    Use @@ Macros

--- plugins/basemap-gmaps-gray.js
+++ plugins/basemap-gmaps-gray.js
@@ -2,9 +2,11 @@
 // @id             iitc-plugin-basemap-gmaps-gray@jacob1123
 // @name           IITC plugin: Gray Google Roads
 // @category       Map Tiles
-// @version        0.1.0.20130724.0
+// @version        0.1.0.@@DATETIMEVERSION@@
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
-// @description    Add a simplified gray Version of Google map tiles as an optional layer
+// @updateURL      @@UPDATEURL@@
+// @downloadURL    @@DOWNLOADURL@@
+// @description    [@@BUILDNAME@@-@@BUILDDATE@@] Add a simplified gray Version of Google map tiles as an optional layer
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
@@ -12,38 +14,21 @@
 // @grant          none
 // ==/UserScript==
 
+@@PLUGINSTART@@
 
-function wrapper() {
-	// ensure plugin framework is there, even if iitc is not yet loaded
-	if(typeof window.plugin !== 'function') window.plugin = function() {};
+// PLUGIN START ////////////////////////////////////////////////////////
 
-    // PLUGIN START ////////////////////////////////////////////////////////
-    
-    // use own namespace for plugin
-    window.plugin.grayGMaps = function() {};
-	window.plugin.grayGMaps.addLayer = function() {
-        var grayGMaps = new L.Google('ROADMAPS',{maxZoom:20});
-        grayGMaps._styles = [{featureType:"landscape.natural",stylers:[{visibility:"simplified"},{saturation:-100},{lightness:-80},{gamma:2.44}]},{featureType:"road",stylers:[{visibility:"simplified"},{color:"#bebebe"},{weight:.6}]},{featureType:"poi",stylers:[{saturation:-100},{visibility:"on"},{gamma:.14}]},{featureType:"water",stylers:[{color:"#32324f"}]},{featureType:"transit",stylers:[{visibility:"off"}]},{featureType:"road",elementType:"labels",stylers:[{visibility:"off"}]},{featureType:"poi",elementType:"labels",stylers:[{visibility:"off"}]},{featureType:"poi"},{featureType:"landscape.man_made",stylers:[{saturation:-100},{gamma:.13}]},{featureType:"water",elementType:"labels",stylers:[{visibility:"off"}]}]
+// use own namespace for plugin
+window.plugin.grayGMaps = function() {};
+window.plugin.grayGMaps.addLayer = function() {
+    var grayGMaps = new L.Google('ROADMAPS',{maxZoom:20});
+    grayGMaps._styles = [{featureType:"landscape.natural",stylers:[{visibility:"simplified"},{saturation:-100},{lightness:-80},{gamma:2.44}]},{featureType:"road",stylers:[{visibility:"simplified"},{color:"#bebebe"},{weight:.6}]},{featureType:"poi",stylers:[{saturation:-100},{visibility:"on"},{gamma:.14}]},{featureType:"water",stylers:[{color:"#32324f"}]},{featureType:"transit",stylers:[{visibility:"off"}]},{featureType:"road",elementType:"labels",stylers:[{visibility:"off"}]},{featureType:"poi",elementType:"labels",stylers:[{visibility:"off"}]},{featureType:"poi"},{featureType:"landscape.man_made",stylers:[{saturation:-100},{gamma:.13}]},{featureType:"water",elementType:"labels",stylers:[{visibility:"off"}]}]
 
-  		layerChooser.addBaseLayer(grayGMaps, "Google Gray");
-	};
+		layerChooser.addBaseLayer(grayGMaps, "Google Gray");
+};
 
-	var setup =  window.plugin.grayGMaps.addLayer;
+var setup =  window.plugin.grayGMaps.addLayer;
 	
-    // PLUGIN END //////////////////////////////////////////////////////////
+// PLUGIN END //////////////////////////////////////////////////////////
 
-    if(window.iitcLoaded && typeof setup === 'function') {
-        setup();
-    } else {
-        if(window.bootPlugins)
-            window.bootPlugins.push(setup);
-        else
-            window.bootPlugins = [setup];
-    }
-} // wrapper end
-
-// inject code into site context
-var script = document.createElement('script');
-script.appendChild(document.createTextNode('('+ wrapper +')();'));
-(document.body || document.head || document.documentElement).appendChild(script);
-
+@@PLUGINEND@@
