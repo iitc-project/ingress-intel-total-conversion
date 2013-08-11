4b7a19a2c6d5534a60f2761b27042707ee0e0bc2
Giacinto Garcea <info@giacintogarcea.com>
2013-08-11 17:46:54 +0200

    Changed description

--- plugins/data-freezer.user.js
+++ plugins/data-freezer.user.js
@@ -6,7 +6,7 @@
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      @@UPDATEURL@@
 // @downloadURL    @@DOWNLOADURL@@
-// @description    [@@BUILDNAME@@-@@BUILDDATE@@] Disable automatic portal data refresh.
+// @description    [@@BUILDNAME@@-@@BUILDDATE@@] Disables the automatic data refresh.
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
