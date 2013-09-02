fe632b5cccbfb44e310119106cf0930f56ebd46a
Philipp Schaefer <trashzopf@gmail.com>
2013-09-02 21:27:16 +0200

    added method to get versionCode of mobile app to JSInterface

--- mobile/plugins/user-location.user.js
+++ mobile/plugins/user-location.user.js
@@ -47,6 +47,8 @@ window.plugin.userLocation.setup = function() {
 
 window.plugin.userLocation.updateLocation = function(lat, lng) {
     var latlng = new L.LatLng(lat, lng);
+    var foo = android.getVersionCode();
+    window.console.log('version code: ' + foo);
     window.plugin.userLocation.marker.setLatLng(latlng);
 }
 
