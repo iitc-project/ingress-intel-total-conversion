d0a35ac4f10938630c6c02348f3a6a781bf7affd
Jon Atkins <github@jonatkins.com>
2013-09-17 02:27:06 +0100

    new plugin, for use on unattended display screens. causes idle mode to be left once per hour, refreshing chat and map data

---
+++ plugins/periodic-refresh.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @id             iitc-plugin-periodic-refresh
+// @name           IITC plugin: Periodic refresh
+// @category       Tweaks
+// @version        0.1.0.@@DATETIMEVERSION@@
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      @@UPDATEURL@@
+// @downloadURL    @@DOWNLOADURL@@
+// @description    For use for unattended display screens only, this plugin causes idle mode to be left once per hour.
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @grant          none
+// ==/UserScript==
+
+@@PLUGINSTART@@
+
+// PLUGIN START ////////////////////////////////////////////////////////
+
+window.plugin.periodicRefresh = function() {};
+
+window.plugin.periodicRefresh.wakeup = function() {
+  console.log('periodicRefresh: timer fired - leaving idle mode');
+  idleReset();
+}
+
+
+window.plugin.periodicRefresh.setup = function() {
+
+  var refreshMinutes = 60;
+
+  setInterval ( window.plugin.periodicRefresh.wakeup, refreshMinutes*60*1000 );
+
+};
+
+var setup = window.plugin.periodicRefresh.setup;
+
+// PLUGIN END //////////////////////////////////////////////////////////
+
+@@PLUGINEND@@
