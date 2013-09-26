1a784e04324d4affc0cfabd3dfac2f0d558c4086
amsdams <julien.langeveld@gmail.com>
2013-09-26 10:10:17 +0200

    code polish, no new features
    added some CONSTANTS for plugin scope and removed 'reset'. versionnumber
    updated

--- plugins/portal-highlighter-missing-mods.user.js
+++ plugins/portal-highlighter-missing-mods.user.js
@@ -2,7 +2,7 @@
 // @id             iitc-plugin-highlight-portals-missing-mods@amsdams
 // @name           IITC plugin: highlight portals missing mods
 // @category       Highlighter
-// @version        0.1.1.@@DATETIMEVERSION@@
+// @version        0.1.2.@@DATETIMEVERSION@@
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      @@UPDATEURL@@
 // @downloadURL    @@DOWNLOADURL@@
@@ -17,12 +17,13 @@
 //PLUGIN START ////////////////////////////////////////////////////////
 //use own namespace for plugin
 window.plugin.portalHighligherPortalsMissingMods = function () {};
+window.plugin.portalHighligherPortalsMissingMods.MODS_PER_PORTAL=4;
+window.plugin.portalHighligherPortalsMissingMods.PORTAL_FILL_COLOR='red';
+window.plugin.portalHighligherPortalsMissingMods.PORTAL_FILL_OPACITY=0.7;
+
 window.plugin.portalHighligherPortalsMissingMods.highlight = function (data, missing) {
-  var d = data.portal.options.details,
-    mods = d.portalV2.linkedModArray,
-    countMissing = 0,
-    opacity = 0.7,
-    color = 'red';
+  var mods = data.portal.options.details.portalV2.linkedModArray,
+    countMissing = 0;
   $.each(mods, function (ind, mod) {
     if(!mod) {
       countMissing++;
@@ -30,14 +31,8 @@ window.plugin.portalHighligherPortalsMissingMods.highlight = function (data, mis
   });
   if(countMissing === missing) {
     data.portal.setStyle({
-      fillColor: color,
-      fillOpacity: opacity
-    });
-  } else {
-    // reset
-    data.portal.setStyle({
-      color: window.COLORS[getTeam(data.portal.options.details)],
-      fillOpacity: 0.5
+      fillColor: window.plugin.portalHighligherPortalsMissingMods.PORTAL_FILL_OPACITY,
+      fillOpacity: window.plugin.portalHighligherPortalsMissingMods.PORTAL_FILL_COLOR
     });
   }
 }
@@ -47,7 +42,7 @@ window.plugin.portalHighligherPortalsMissingMods.getHighlighter = function (miss
   });
 }
 var setup = function () {
-  for(var missing = 1; missing < 5; missing++) {
+  for(var missing = 1; missing <= window.plugin.portalHighligherPortalsMissingMods.MODS_PER_PORTAL; missing++) {
     window.addPortalHighlighter('Mods Missing: ' + missing + ' mods ', window.plugin.portalHighligherPortalsMissingMods.getHighlighter(missing));
   }
 }
