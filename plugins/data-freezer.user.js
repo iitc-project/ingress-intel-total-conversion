6a8f551d92c68c9113a3fc5aa818ac363034e5c9
Giacinto Garcea <info@giacintogarcea.com>
2013-09-27 00:47:58 +0200

    v1.4 Fixed bug about chat data restart
    Added icon to the button

--- plugins/data-freezer.user.js
+++ plugins/data-freezer.user.js
@@ -2,7 +2,7 @@
 // @id             iitc-plugin-data-freezer@zaso
 // @name           IITC plugin: Data Freezer
 // @category       Tweaks
-// @version        0.1.3.@@DATETIMEVERSION@@
+// @version        0.1.4.@@DATETIMEVERSION@@
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      @@UPDATEURL@@
 // @downloadURL    @@DOWNLOADURL@@
@@ -31,45 +31,44 @@
       window.plugin.dataFreezer.status = true;
       localStorage[window.plugin.dataFreezer.storageKEY] = 1;
 
-      // Freezes the data map
+      // Freezes map data refresh
       window.mapDataRequest.MAX_REQUESTS = window.plugin.dataFreezer.noMoreData[0];
       window.mapDataRequest.REFRESH_CLOSE = window.plugin.dataFreezer.noMoreData[1];
 
-      // Freezes the chat
+      // Freezes chat data refresh
       window.CHAT_PUBLIC_ITEMS = window.plugin.dataFreezer.noMoreData[0];
       window.CHAT_FACTION_ITEMS = window.plugin.dataFreezer.noMoreData[0];
-      chat.requestFaction(false);
-      chat.requestPublic(false);
+
+      $('a.dataFreezerBtn').addClass('noData');
+      $('#updatestatus #innerstatus').hide();
+      $('#updatestatus #datafreezer').show();
     } else {
       window.plugin.dataFreezer.status = false;
       localStorage[window.plugin.dataFreezer.storageKEY] = 0;
 
-      // Starts the data map
+      // Re-starts map data refresh
       window.mapDataRequest.MAX_REQUESTS = window.plugin.dataFreezer.defaultVal[0];
       window.mapDataRequest.REFRESH_CLOSE = window.plugin.dataFreezer.defaultVal[1];
 
-      // Starts the chat
-      window.CHAT_PUBLIC_ITEMS = window.plugin.dataFreezer.defaultVal[3];
-      window.CHAT_FACTION_ITEMS = window.plugin.dataFreezer.defaultVal[4];
-      chat.requestFaction(true);
-      chat.requestPublic(true);
+      // Re-starts chat data refresh
+      window.CHAT_PUBLIC_ITEMS = window.plugin.dataFreezer.defaultVal[2];
+      window.CHAT_FACTION_ITEMS = window.plugin.dataFreezer.defaultVal[3];
+
+      $('a.dataFreezerBtn').removeClass('noData');;
+      $('#updatestatus #innerstatus').show();
+      $('#updatestatus #datafreezer').hide();
     }
-    window.plugin.dataFreezer.setupCSS();
+
     // Creates a movement on the map to refresh it
     map.zoomOut(1, {animate:false});
     map.zoomIn(1, {animate:false});
   }
 
   window.plugin.dataFreezer.setupCSS = function() {
-    if(window.plugin.dataFreezer.status === true) {
-      $('a.dataFreezer').css({'background':'#f66','outline':'none'});
-      $('#updatestatus #innerstatus').hide();
-      $('#updatestatus #datafreezer').show();
-    } else {
-      $('a.dataFreezer').css({'background':'#03DC03','outline':'none'});
-      $('#updatestatus #innerstatus').show();
-      $('#updatestatus #datafreezer').hide();
-    }
+    $("<style>").prop("type", "text/css").html(''
+      +'.leaflet-dataFreezer a.dataFreezerBtn, .leaflet-dataFreezer a.dataFreezerBtn:hover{outline:none;background:#03DC03 url(@@INCLUDEIMAGE:images/refresh-icon.png@@) no-repeat center center;}'
+      +'.leaflet-dataFreezer a.dataFreezerBtn.noData, .leaflet-dataFreezer a.dataFreezerBtn.noData:hover{background-color:#f66;}'
+    ).appendTo("head");
   }
 
   var setup = function() {
@@ -79,7 +78,7 @@
       onAdd:function(map) {
         var controlDiv = L.DomUtil.create('div', 'leaflet-dataFreezer leaflet-control');
         var controlSubDIV = L.DomUtil.create('div', 'leaflet-bar', controlDiv);
-        var butt = L.DomUtil.create('a', 'dataFreezer', controlSubDIV);
+        var butt = L.DomUtil.create('a', 'dataFreezerBtn', controlSubDIV);
         butt.title = 'Enables/Disables the data refresh';
 
         L.DomEvent
@@ -99,15 +98,19 @@
 
     $('#updatestatus #innerstatus').after('<div id="datafreezer"><b>map</b>: freezed</div>');
 
+    window.plugin.dataFreezer.setupCSS();
+
     if(!localStorage[window.plugin.dataFreezer.storageKEY]) {
       localStorage[window.plugin.dataFreezer.storageKEY] = 1;
     }
     if(localStorage[window.plugin.dataFreezer.storageKEY] === '1') {
+      // Disables data refresh
       window.plugin.dataFreezer.status = false;
-      window.plugin.dataFreezer.toggle();
     } else {
-      window.plugin.dataFreezer.setupCSS();
+      // Enables data refresh
+      window.plugin.dataFreezer.status = true;
     }
+    window.plugin.dataFreezer.toggle();
   }
 
 // PLUGIN END //////////////////////////////////////////////////////////
