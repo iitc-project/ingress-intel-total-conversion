89e43734fb8d3df9ee6899532772200c331c785d
fkloft <felix.kloft+github@gmail.com>
2013-12-01 21:43:55 +0100

    Bump versions

--- plugins/guess-player-levels.user.js
+++ plugins/guess-player-levels.user.js
@@ -2,7 +2,7 @@
 // @id             iitc-plugin-guess-player-levels@breunigs
 // @name           IITC plugin: guess player level
 // @category       Info
-// @version        0.4.10.@@DATETIMEVERSION@@
+// @version        0.5.0.@@DATETIMEVERSION@@
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      @@UPDATEURL@@
 // @downloadURL    @@DOWNLOADURL@@
