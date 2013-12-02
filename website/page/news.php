689350bd3f60bc9c5a762d387ba605ff9a86911b
Jon Atkins <github@jonatkins.com>
2013-12-02 01:24:58 +0000

    website: add news of new release

--- website/page/news.php
+++ website/page/news.php
@@ -1,6 +1,24 @@
 <h2>News</h2>
 
-<div class="alert alert-block alert-error">
+<h4>2nd December 2013</h3>
+<p>
+IITC 0.16.0 and IITC Mobile 0.10.0 have been released. This update is required to work with the latest changes to
+the standard intel site. This update took a fair amount of work due to major changes
+in the network protocol used behind the standard intel website, hence the longer than usual delay for the update.
+</p>
+<p>
+As well as IITC itself, nearly every single plugin broke in some way due to these changes. Due to the amount of work
+needed to get everything possible working again, some plugins have been disabled for now. You can see the list of these
+disabled plugins in the download list - they're in the 'Deleted' category with a description of 'PLUGIN CURRENTLY UNAVAILABLE'.
+</p>
+<p>
+Shortly after the Niantic changes that broke IITC, there were reports of IITC users being banned. This seemed strange at
+first, as IITC was not even functioning at this time, so why would people be using it and getting banned. The conclusion
+that was reached was that a few people who tried to use the broken IITC Mobile app triggered either a bug in IITC that
+caused excessive requests, or triggered some kind of alert in the intel servers. Changes have been made to IITC now
+so this unlikely to be an issue again.
+</p>
+
 <h4>27th November 2013</h4>
 <p>
 IITC and IITC Mobile are currently broken, due to changes made to the standard intel website. This is a major change in how
@@ -23,7 +41,6 @@ More details, and discussion, available in the
 This is shown as version 0.15.99. When a fixed build is released, it will be 0.16.something and will update and start working.
 Test versions remain, but broken. Please join the Google+ Community where announcements will be made.
 </p>
-</div>
 
 <h4>11th November 2013</h4>
 <p>
