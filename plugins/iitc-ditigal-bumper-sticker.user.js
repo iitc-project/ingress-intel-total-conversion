83c5972502b3890192603f82a5a92adbd82bebe6
Jon Atkins <github@jonatkins.com>
2013-12-15 02:38:57 +0000

    digital bumper sticker - use smaller logo, fine tune position - change z-index so it doesn't cover up the detailed AP tooltip

--- plugins/iitc-ditigal-bumper-sticker.user.js
+++ plugins/iitc-ditigal-bumper-sticker.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @id             iitc-plugin-portals-list@teo96
+// @id             iitc-digital-bumper-sticker
 // @name           IITC Digital Bumper Sticker
 // @category       Stock
-// @version        0.1.0
+// @version        0.1.0.@@DATETIMEVERSION@@
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      @@UPDATEURL@@
 // @downloadURL    @@DOWNLOADURL@@
@@ -15,10 +15,10 @@
 // ==/UserScript==
 
 var logoDiv = document.createElement('div');
-logoDiv.setAttribute('style', "position: fixed; left: 30px; top: 130px; z-index: 100; pointer-events: none;");
+logoDiv.setAttribute('style', "position: fixed; left: 20px; top: 130px; z-index: auto; pointer-events: none;");
 
 var img = document.createElement('img');
-img.setAttribute('src', 'http://iitc.jonatkins.com/assets/img/prefer-iitc-small.png');
+img.setAttribute('src', 'http://iitc.jonatkins.com/assets/img/prefer-iitc-200.png');
 
 logoDiv.appendChild(img);
 
