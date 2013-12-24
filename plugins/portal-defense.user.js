65577205492a6454d871072324316ab803910d1a
Jon Atkins <github@jonatkins.com>
2013-12-24 04:53:35 +0000

    portal-defense plugin: permanant removal

--- plugins/portal-defense.user.js
+++ plugins/portal-defense.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @id             iitc-plugin-defense@gluckies
 // @name           IITC plugin: portal defense 
-// @category  Deleted
+// @category       Deleted
 // @version        0.2.2.@@DATETIMEVERSION@@
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      @@UPDATEURL@@
 // @downloadURL    @@DOWNLOADURL@@
-// @description   PLUGIN CURRENTLY UNAVAILABLE
+// @description    The data this plugin needs is no longer available since the late November 2013 intel site update.
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
