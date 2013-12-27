3fe9b91a92098a998c74752fc5ef99ae2cb18e87
Philipp Schaefer <trashzopf@gmail.com>
2013-12-27 17:09:33 +0100

    use current best location estimation from the android docs: http://developer.android.com/guide/topics/location/strategies.html#BestEstimate

--- mobile/src/com/cradle/iitc_mobile/IITC_UserLocation.java
+++ mobile/src/com/cradle/iitc_mobile/IITC_UserLocation.java
@@ -136,6 +136,65 @@ public class IITC_UserLocation implements LocationListener, SensorEventListener 
         mIitc.invalidateOptionsMenu();
     }
 
+    private static final int TWO_MINUTES = 1000 * 60 * 2;
+
+    /**
+      * Determines whether one Location reading is better than the current Location fix
+      * @param location  The new Location that you want to evaluate
+      * @param currentBestLocation  The current Location fix, to which you want to compare the new one
+      *
+      * code copied from http://developer.android.com/guide/topics/location/strategies.html#BestEstimate
+      */
+    protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+        if (currentBestLocation == null) {
+            // A new location is always better than no location
+            return true;
+        }
+
+        // Check whether the new location fix is newer or older
+        long timeDelta = location.getTime() - currentBestLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+        boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+        boolean isNewer = timeDelta > 0;
+
+        // If it's been more than two minutes since the current location, use the new location
+        // because the user has likely moved
+        if (isSignificantlyNewer) {
+            return true;
+        // If the new location is more than two minutes older, it must be worse
+        } else if (isSignificantlyOlder) {
+            return false;
+        }
+
+        // Check whether the new location fix is more or less accurate
+        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 100;
+
+        // Check if the old and new location are from the same provider
+        boolean isFromSameProvider = isSameProvider(location.getProvider(),
+                currentBestLocation.getProvider());
+
+        // Determine location quality using a combination of timeliness and accuracy
+        if (isMoreAccurate) {
+            return true;
+        } else if (isNewer && !isLessAccurate) {
+            return true;
+        } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+            return true;
+        }
+        return false;
+    }
+
+    // Checks whether two providers are the same
+    private boolean isSameProvider(String provider1, String provider2) {
+        if (provider1 == null) {
+          return provider2 == null;
+        }
+        return provider1.equals(provider2);
+    }
+
     /**
      * set the location mode to use. Available modes:
      * 0: don't show user's position
@@ -157,10 +216,9 @@ public class IITC_UserLocation implements LocationListener, SensorEventListener 
 
     @Override
     public void onLocationChanged(Location location) {
-        mLastLocation = location;
+        if (!isBetterLocation(location, mLastLocation)) return;
 
-        // throw away all positions with accuracy > 100 meters should avoid gps glitches
-        if (location.getAccuracy() > 100) return;
+        mLastLocation = location;
 
         // do not touch the javascript while iitc boots
         if (mIitc.isLoading()) return;
