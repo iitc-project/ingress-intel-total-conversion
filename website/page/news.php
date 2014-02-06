3fce1628884e389a9227b5a794cb1036c7847093
Jon Atkins <github@jonatkins.com>
2014-02-06 17:50:28 +0000

    website: update for new 0.16.4 release

--- website/page/news.php
+++ website/page/news.php
@@ -1,5 +1,31 @@
 <h2>News</h2>
 
+<h4>6th February 2014</h4>
+<p>
+IITC 0.16.4 and IITC Mobile 0.10.4 have just been released. This version is required to fix a bug with showing portal details
+due to a change made by Niantic to the intel site protocol. Also, the following changes have been made:
+</p>
+<ul>
+<li>Portal markers are now reduced in size when you zoom out, reducing clutter when viewing large areas of the map</li>
+<li>Blocked a 3rd party plugin, arc, from running - it had spyware features hidden within it
+(<a href="https://plus.google.com/105383756361375410867/posts/4b2EjP3Du42">details here</a>).</li>
+<li>Plugins
+ <ul>
+ <li>add-kml: support for opening files on mobile added</li>
+ <li>regions: new plugin to draw the scoreboard regions on the map. <i>No support for showing scores - this needs Niantic to add it to the standard intel site first</i></li>
+ <li>score-cycle-times: new plugin to show the times of the scoreboard cycles</li>
+ <li>draw-tools: added basic import/export (via copy+paste), and colour choosing options (click on "DrawTools Opt" in the sidebar)</li>
+ <li>compute-ap-stats and portal-names: changed code to reduce the performance impact when a large number of portals are shown</li>
+ </ul>
+</li>
+<li>Mobile:
+ <ul>
+ <li>NFC support for sharing map view/selected portal - app permissions updated for this</li>
+ </ul>
+</li>
+<li>.. plus various minor bugfixes and improvements</li>
+</ul>
+
 <h4>13th January 2014</h4>
 <p>
 A new IITC release, 0.16.2 and IITC Mobile 0.10.2 have been released. These are needed to work with a change to the
