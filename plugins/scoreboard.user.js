7297d096627e0e288d868407b251f5c7e4b267ee
Costaspap <costaspapastamos@hotmail.com>
2014-06-10 00:38:59 +0300

    completed build

--- plugins/scoreboard.user.js
+++ plugins/scoreboard.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @id             iitc-plugin-scoreboard@Costaspap
+// @id             iitc-plugin-scoreboard@vita10gy
 // @name           IITC plugin: show a localized scoreboard.
 // @version        0.2.0.@@DATETIMEVERSION@@
 // @category       Info
@@ -13,20 +13,9 @@
 // @match          http://www.ingress.com/intel*
 // @grant          none
 // ==/UserScript==
-
-
-// A plug in by Costaspap and harisbitsakou
-function wrapper(plugin_info) {
-    // ensure plugin framework is there, even if iitc is not yet loaded
-    if(typeof window.plugin !== 'function') window.plugin = function() {};
     
-    //PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
-    //(leaving them in place might break the 'About IITC' page or break update checks)
-    plugin_info.buildName = 'jonatkins';
-    plugin_info.dateTimeVersion = '20140524.214738';
-    plugin_info.pluginId = 'scoreboard';
-    //END PLUGIN AUTHORS NOTE
-    
+	
+	@@PLUGINSTART@@
     // PLUGIN START //
     
     // use own namespace for plugin
@@ -293,18 +282,6 @@ function wrapper(plugin_info) {
     
     
     // PLUGIN END //////////////////////////////////////////////////////////
-    
-    
-    setup.info = plugin_info; //add the script info data to the function as a property
-    if(!window.bootPlugins) window.bootPlugins = [];
-    window.bootPlugins.push(setup);
-    // if IITC has already booted, immediately run the 'setup' function
-    if(window.iitcLoaded && typeof setup === 'function') setup();
-} // wrapper end
-// inject code into site context
-var script = document.createElement('script');
-var info = {};
-if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) info.script = { version: GM_info.script.version, name: GM_info.script.name, description: GM_info.script.description };
-script.appendChild(document.createTextNode('('+ wrapper +')('+JSON.stringify(info)+');'));
-(document.body || document.head || document.documentElement).appendChild(script);
+    @@PLUGINEND@@
+
     
