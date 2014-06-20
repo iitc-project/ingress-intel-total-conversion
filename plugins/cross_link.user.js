a2db58f494bfcb8669f7a7ec45ce94c7549e8e3a
Jon Atkins <github@jonatkins.com>
2014-06-20 23:06:18 +0100

    fix description in new plugin (macros, and modified the text. marked as 'EXPERIMENTAL' for now until i've done additional work)

--- plugins/cross_link.user.js
+++ plugins/cross_link.user.js
@@ -6,7 +6,7 @@
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      @@UPDATEURL@@
 // @downloadURL    @@DOWNLOADURL@@
-// @description    [McBen] requires drawtool! coloring of link collision
+// @description    [@@BUILDNAME@@-@@BUILDDATE@@] EXPERIMENTAL: Checks for existing links that cross planned links. Requires draw-tools plugin.
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
