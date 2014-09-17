0d218705eae09e7e23b9b63353081436bcac4adc
Jon Atkins <github@jonatkins.com>
2014-09-17 21:29:22 +0100

    update website for new release

--- website/page/home.php
+++ website/page/home.php
@@ -13,27 +13,18 @@ offers many more features. It is available for
 
 <h3>Latest news</h3>
 
-<h4>15th August 2014</h4>
+<h4>17th September 2014</h4>
 <p>
-IITC 0.17.5 and IITC Mobile 0.11.5 have just been released. This is a critical update required to work
-with the latest changes to the standard intel site.
+IITC 0.18.0 and IITC Mobile 0.11.6 have just been released. This is an important update required to fix a few
+problems caused in recent Niantic site updates.
 </p>
-<p>
-Along with the fixes needed to work with the Niantic changes, the only other change since the previous
-release is a minor colour scheme change for mods, to match the standard intel site. Rare is now a
-shade of blue (rather than purple), and very rare is now purple (rather than pink).
-</p>
-
-<h4>14th August 2014</h4>
-<p>
-IITC 0.17.4 and IITC Mobile 0.11.4 have just been released. Changes include:
-</p>
+<p>Changes include:</p>
 <ul>
-<li>Support for the Helios artifacts</li>
-<li>New plugin: 'uniques' - allows manual and partial automatic tracking of unique portals visited/captured</li>
-<li>Passcodes can be redeemed - enter them in the box below the portal details in the sidebar</li>
-<li>Big improvements in network error handling - including a new prompt when IITC needs a reload after a Niantic site update</li>
-<li>and, as usual, other tweaks and bugfixes</li>
+<li>New plugin: layer count. Counts the number of overlapping layers on the map at the selected point</li>
+<li>Portals list plugin: A major update, including integration with the bookmarks and uniques plugins</li>
+<li>COMM bugfixes: only load the visible tab (in preperation for adding the 'alerts' tab), and fix COMM not loading on global map view</li>
+<li>plus other tweaks and bugfixes...</li>
 </ul>
+
 
 <a class="btn btn-default btn-sm" href="?page=news">Older news</a>
