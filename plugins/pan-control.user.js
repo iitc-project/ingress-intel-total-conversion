118ffd5cfeb001afa775481a95633a55709e71bf
Mike Castle <dalgoda@gmail.com>
2014-11-16 19:48:58 -0800

    Replace use of L.Point() with arrays.  These can be updated on the fly and function the same way.

--- plugins/pan-control.user.js
+++ plugins/pan-control.user.js
@@ -22,6 +22,19 @@
 // use own namespace for plugin
 window.plugin.panControl = function() {};
 
+window.plugin.panControl.up = [0, 0];
+window.plugin.panControl.down = [0, 0];
+window.plugin.panControl.left = [0, 0];
+window.plugin.panControl.right = [0, 0];
+
+window.plugin.panControl.setButtons = function() {
+  var opts = window.map.panControl.options;
+  window.plugin.panControl.up[1] = -opts.panVerticalOffset;
+  window.plugin.panControl.down[1] = opts.panVerticalOffset;
+  window.plugin.panControl.left[0] = -opts.panHorizontalOffset;
+  window.plugin.panControl.right[0] = opts.panHorizontalOffset;
+}
+
 window.plugin.panControl.setup  = function() {
   try { console.log('Loading Leaflet.Pancontrol JS now'); } catch(e) {}
   @@INCLUDERAW:external/L.Control.Pan.js@@
@@ -32,8 +45,38 @@ window.plugin.panControl.setup  = function() {
     panControl: false
   });
 
+  L.Control.Pan2 = L.Control.Pan.extend({
+    options: {
+      position: 'topleft',
+      panVerticalOffset: 500,
+      panHorizontalOffset: 500
+    },
 
-  window.map.panControl = L.control.pan({panOffset: 350});
+    onAdd: function (map) {
+      var className = 'leaflet-control-pan',
+        container = L.DomUtil.create('div', className);
+
+      window.plugin.panControl.setButtons();
+
+      this._panButton('Up'   , className + '-up'
+              , container, map, window.plugin.panControl.up);
+      this._panButton('Left' , className + '-left'
+              , container, map, window.plugin.panControl.left);
+      this._panButton('Right', className + '-right'
+              , container, map, window.plugin.panControl.right);
+      this._panButton('Down' , className + '-down'
+              , container, map, window.plugin.panControl.down);
+
+      return container;
+    },
+
+  });
+
+  L.control.pan2 = function(options) {
+    return new L.Control.Pan2(options);
+  }
+
+  window.map.panControl = L.control.pan2({panVerticalOffset: 350});
   window.map.addControl(window.map.panControl);
 
   if(map.zoomControl._map) {  // Move above the zoom control
