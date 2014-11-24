3664a8d114079a83c569c10900f61046765a8eaa
Umer Salman <umer936@gmail.com>
2014-11-24 11:05:39 -0600

    Revert "Waze Maps link"
    This reverts commit 6837fb17257b49436f6009e1cf5c2139b566e41d.

--- code/utils_misc.js
+++ code/utils_misc.js
@@ -205,12 +205,11 @@ window.showPortalPosLinks = function(lat, lng, name) {
     var qrcode = '<div id="qrcode"></div>';
     var script = '<script>$(\'#qrcode\').qrcode({text:\'GEO:'+lat+','+lng+'\'});</script>';
     var gmaps = '<a href="https://maps.google.com/maps?ll='+lat+','+lng+'&q='+lat+','+lng+'%20('+encoded_name+')">Google Maps</a>';
-    var wazemaps = '<a href="https://www.waze.com/livemap?zoom=17&lat='+lat+'&lon='+lng+'">Waze Maps</a>';
     var bingmaps = '<a href="http://www.bing.com/maps/?v=2&cp='+lat+'~'+lng+'&lvl=16&sp=Point.'+lat+'_'+lng+'_'+encoded_name+'___">Bing Maps</a>';
     var osm = '<a href="http://www.openstreetmap.org/?mlat='+lat+'&mlon='+lng+'&zoom=16">OpenStreetMap</a>';
     var latLng = '<span>&lt;' + lat + ',' + lng +'&gt;</span>';
     dialog({
-      html: '<div style="text-align: center;">' + qrcode + script + gmaps + '; ' + bingmaps + '; ' + wazemaps + '; ' + osm + '<br />' + latLng + '</div>',
+      html: '<div style="text-align: center;">' + qrcode + script + gmaps + '; ' + bingmaps + '; ' + osm + '<br />' + latLng + '</div>',
       title: name,
       id: 'poslinks'
     });
