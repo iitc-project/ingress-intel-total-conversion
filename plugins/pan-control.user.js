fda2d403ebcf55deda5732be2cc27a3c6a5fe9de
Mike Castle <dalgoda@gmail.com>
2014-12-14 23:31:49 -0800

    Register a new UI to control how far to pan when you click the buttons.

--- plugins/pan-control.user.js
+++ plugins/pan-control.user.js
@@ -26,19 +26,136 @@ window.plugin.panControl.up = [0, 0];
 window.plugin.panControl.down = [0, 0];
 window.plugin.panControl.left = [0, 0];
 window.plugin.panControl.right = [0, 0];
+window.plugin.panControl.horizontalKey = 'plugin-pan-control-horizontal';
+window.plugin.panControl.verticalKey = 'plugin-pan-control-vertical';
+
+window.plugin.panControl.saveState = function(key, state) {
+  localStorage[key] = JSON.stringify(state);
+}
+
+window.plugin.panControl.loadState = function(key) {
+  state = {
+    mode: 'absolute',
+    absolute: 350,
+    relative: 50,
+    };
+
+  var saved = localStorage[key];
+  if (saved) {
+    state = JSON.parse(saved);
+  } else {
+    window.plugin.panControl.saveState(key, state);
+  }
+  return state;
+}
+
+window.plugin.panControl.getOffset = function(key, pixel_count) {
+  var state = window.plugin.panControl.loadState(key);
+  var offset = state.absolute;
+  if (state.mode === 'relative') {
+    offset = pixel_count * state.relative / 100;
+  }
+  return offset;
+}
 
 window.plugin.panControl.setButtons = function() {
-  var opts = window.map.panControl.options;
-  window.plugin.panControl.up[1] = -opts.panVerticalOffset;
-  window.plugin.panControl.down[1] = opts.panVerticalOffset;
-  window.plugin.panControl.left[0] = -opts.panHorizontalOffset;
-  window.plugin.panControl.right[0] = opts.panHorizontalOffset;
+  var mapSize = window.map.getSize();
+  var hOffset = window.plugin.panControl.getOffset(
+    window.plugin.panControl.horizontalKey,
+    mapSize.x);
+  var vOffset = window.plugin.panControl.getOffset(
+    window.plugin.panControl.verticalKey,
+    mapSize.y);
+
+  window.plugin.panControl.up[1] = -vOffset;
+  window.plugin.panControl.down[1] = vOffset;
+  window.plugin.panControl.left[0] = -hOffset;
+  window.plugin.panControl.right[0] = hOffset;
+}
+
+window.plugin.panControl.configure = function() {
+  var html = '<div id="pan-control-horizontal"></div>'
+           + '<div id="pan-control-vertical"></div>';
+  dialog({
+    html: html,
+    id: 'pan-control-dialog',
+    dialogClass: 'ui-dialog',
+    title: 'Panning Control Options'
+  });
+  $('#pan-control-horizontal').pan_control({
+    name: 'Horizontal',
+    key: window.plugin.panControl.horizontalKey
+  });
+  $('#pan-control-vertical').pan_control({
+    name: 'Vertical',
+    key: window.plugin.panControl.verticalKey
+  });
 }
 
 window.plugin.panControl.setup  = function() {
   try { console.log('Loading Leaflet.Pancontrol JS now'); } catch(e) {}
   @@INCLUDERAW:external/L.Control.Pan.js@@
   try { console.log('done loading Leaflet.Pancontrol JS'); } catch(e) {}
+
+  $.widget('iitc.pan_control', {
+    options: {
+      name: null,
+      key: null,
+    },
+    max: {
+      absolute: null,
+      relative: 100,
+    },
+    spinner_change: function(e, ui) {
+      var self = e.data;
+      var mode = self.state.mode;
+      self.state[mode] = self.spinner.val();
+      self.update();
+    },
+    select_change: function(e) {
+      var self = e.data;
+      self.state.mode = self.select.val();
+      self.update();
+    },
+    _create: function() {
+      if (!this.options.name) {
+        alert('programming error, name not passed');
+        return;
+      }
+      if (!this.options.key) {
+        alert('programming error, key not passed');
+        return;
+      }
+      this.state = window.plugin.panControl.loadState(this.options.key);
+      var id = this.uuid;
+      var select_id = 'iitc-pan-select-' + id;
+      var spinner_id = 'iitc-pan-spinner-' + id;
+      var selectHtml = '<form>'
+                     + this.options.name
+                     + '  <select id="' + select_id + '">'
+                     + '    <option value="absolute">Absolute (pixels)</option>'
+                     + '    <option value="relative">Relative (percent)</option>'
+                     + '  </select>'
+                     + '  <input name=value id="' + spinner_id + '">'
+                     + '</form>'
+                     ;
+      $(selectHtml).appendTo(this.element);
+      this.select = $('#' + select_id)
+                    .on('change', null, this, this.select_change);
+      this.spinner = $('#' + spinner_id)
+                     .spinner({min: 1})
+                     .on('spinstop', null, this, this.spinner_change);
+      this.update();
+    },
+    update: function() {
+      var mode = this.state.mode;
+      this.select.val(mode);
+      this.spinner.spinner('option', 'max', this.max[mode]);
+      this.spinner.spinner('value', this.state[mode]);
+      window.plugin.panControl.saveState(this.options.key, this.state);
+      window.plugin.panControl.setButtons();
+    },
+  });
 
   // prevent Pancontrol from being activated by default (e.g. in minimap)
   L.Map.mergeOptions({
@@ -48,8 +165,6 @@ window.plugin.panControl.setup  = function() {
   L.Control.Pan2 = L.Control.Pan.extend({
     options: {
       position: 'topleft',
-      panVerticalOffset: 500,
-      panHorizontalOffset: 500
     },
 
     onAdd: function (map) {
@@ -76,7 +191,7 @@ window.plugin.panControl.setup  = function() {
     return new L.Control.Pan2(options);
   }
 
-  window.map.panControl = L.control.pan2({panVerticalOffset: 350});
+  window.map.panControl = L.control.pan2();
   window.map.addControl(window.map.panControl);
 
   if(map.zoomControl._map) {  // Move above the zoom control
@@ -85,8 +200,13 @@ window.plugin.panControl.setup  = function() {
     window.map.addControl(window.map.zoomControl);
   }
 
+  window.map.on('resize', function(e) {
+    window.plugin.panControl.setButtons();
+  });
+
   $('head').append('<style>@@INCLUDESTRING:external/L.Control.Pan.css@@</style>');
-};
+  $('#toolbox').append('<a onclick="window.plugin.panControl.configure();return false;" title="Panning Control Options">Pan Control Opt</a>');
+}
 
 var setup =  window.plugin.panControl.setup;
 
