fa24ae804590aeb844f3bf92c9feea34f0ec3cd2
Jon Atkins <github@jonatkins.com>
2014-12-21 18:17:32 +0000

    website: update for 0.19.0 release

--- website/page/news.php
+++ website/page/news.php
@@ -1,5 +1,20 @@
 <h2>News</h2>
 
+<h4>21st December 2014</h4>
+<p>
+IITC 0.19.0 / IITC Mobile 0.19.0 released.
+</p>
+<p>Changes include</p>
+<ul>
+<li>Version numbers of IITC Mobile have been changed to keep in sync with the main IITC desktop scripts</li>
+<li>Support for regional scores added</li>
+<li>draw-tools: update the copy+paste import/export process to support links as stock intel URL formats, for exchanging plans with non-IITC users.</li>
+<li>IITC Mobile: sometimes the app wouldn't detect the screen size properly on startup, requiring a reload/screen rotation - fixed<./li>
+<li>Fix URL parameter parsing to hanble commas encoded as %3C</li>
+<li>New plugin done-links: a companion to cross-links - it highlights any drawn links that already exist.</li>
+<li>Sync plugin: a couple of bugfixes - it may work better now - not sure if it's 100%</li>
+<li>..and other tweaks/bugfixes, as always</li>
+</ul>
 
 <h4>7th November 2014</h4>
 <p>
