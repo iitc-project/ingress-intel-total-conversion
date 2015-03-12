1dae89b4bc3afeccf140ec28ab5a2a2590b7cc2c
Jon Atkins <github@jonatkins.com>
2015-03-12 20:26:53 +0000

    mission plugin: initial basic highlighter debug highlighter: remove unknown_10/11 highlighters - not needed any more

--- plugins/portal-highlighter-debug.user.js
+++ plugins/portal-highlighter-debug.user.js
@@ -21,35 +21,21 @@
 // use own namespace for plugin
 window.plugin.portalHighlighterDebug = function() {};
 
-window.plugin.portalHighlighterDebug.unknown10 = function(data) {
-  var opacity = 0.7;
-  var color = undefined;
-
-  if (data.portal.options.data.unknown_10) {
-    color='red';
-  }
-
-  if (color) {
-    data.portal.setStyle({fillColor: color, fillOpacity: opacity});
-  }
-}
-
-window.plugin.portalHighlighterDebug.unknown11 = function(data) {
-  var opacity = 0.7;
-  var color = undefined;
-
-  if (data.portal.options.data.unknown_11) {
-    color='red';
-  }
-
-  if (color) {
-    data.portal.setStyle({fillColor: color, fillOpacity: opacity});
-  }
-}
+//window.plugin.portalHighlighterDebug.unknown11 = function(data) {
+//  var opacity = 0.7;
+//  var color = undefined;
+//
+//  if (data.portal.options.data.unknown_11) {
+//    color='red';
+//  }
+//
+//  if (color) {
+//    data.portal.setStyle({fillColor: color, fillOpacity: opacity});
+//  }
+//}
 
 var setup =  function() {
-  window.addPortalHighlighter('DEBUG: Unknoen_10 (mission start portals?)', window.plugin.portalHighlighterDebug.unknown10);
-  window.addPortalHighlighter('DEBUG: Unknoen_11', window.plugin.portalHighlighterDebug.unknown11);
+//  window.addPortalHighlighter('DEBUG: Unknoen_11', window.plugin.portalHighlighterDebug.unknown11);
 }
 
 // PLUGIN END //////////////////////////////////////////////////////////
