e7a136ec049c84a0af4f091b0f4d281b3c292f0d
Jon Atkins <github@jonatkins.com>
2015-03-20 23:33:11 +0000

    bump version after release

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @id             ingress-intel-total-conversion@jonatkins
 // @name           IITC: Ingress intel map total conversion
-// @version        0.22.1.@@DATETIMEVERSION@@
+// @version        0.22.2.@@DATETIMEVERSION@@
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      @@UPDATEURL@@
 // @downloadURL    @@DOWNLOADURL@@
