636e4a5cf210a711adff00691ed1c73d40685ca9
Pål Sollie <sollie@sparkz.no>
2015-06-22 23:39:00 +0200

    Basemap-Kartverket: Added a couple of different tile sets

--- plugins/basemap-kartverket.user.js
+++ plugins/basemap-kartverket.user.js
@@ -6,7 +6,7 @@
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      @@UPDATEURL@@
 // @downloadURL    @@DOWNLOADURL@@
-// @description    [@@BUILDNAME@@-@@BUILDDATE@@] Add the color and grayscale map tiles from Kartverket.no as an optional layer.
+// @description    [@@BUILDNAME@@-@@BUILDDATE@@] Map tiles from Kartverket.no.
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
@@ -30,12 +30,16 @@ window.plugin.mapTileKartverketMap.addLayer = function() {
 
   // Map data from Kartverket (http://statkart.no/en/)
   kartverketAttribution = 'Map data © Kartverket';
-  var kartverketOpt = {attribution: kartverketAttribution, maxNativeZoom: 18, maxZoom: 21, subdomains: ['opencache', 'opencache2', 'opencache3']};
   var kartverketTopo2 = new L.TileLayer('http://{s}.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo2&zoom={z}&x={x}&y={y}', kartverketOpt);
   var kartverketTopo2Grayscale = new L.TileLayer('http://{s}.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo2graatone&zoom={z}&x={x}&y={y}', kartverketOpt);
-
+  var kartverketBasemap = new L.TileLayer('http://{s}.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=norges_grunnkart&zoom={z}&x={x}&y={y}', kartverketOpt);
+  var kartverketBasemapGrayscale = new L.TileLayer('http://{s}.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=norges_grunnkart_graatone&zoom={z}&x={x}&y={y}', kartverketOpt);
+    
+  layerChooser.addBaseLayer(kartverketBasemap, "Norway Basemap");
+  layerChooser.addBaseLayer(kartverketBasemapGrayscale, "Norway Basemap Grayscale");
   layerChooser.addBaseLayer(kartverketTopo2, "Norway Topo");
   layerChooser.addBaseLayer(kartverketTopo2Grayscale, "Norway Topo Grayscale");
+
 };
 
 var setup =  window.plugin.mapTileKartverketMap.addLayer;
