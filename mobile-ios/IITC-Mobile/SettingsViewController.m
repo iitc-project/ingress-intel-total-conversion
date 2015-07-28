e6949f7ffdd52425ad51e0eaa92907d672e6e1ca
Hubertzhang <hubert.zyk@gmail.com>
2015-07-28 17:55:51 +0800

    add a settings bundle

---
+++ mobile-ios/IITC-Mobile/SettingsViewController.m
@@ -0,0 +1,37 @@
+//
+//  SettingsViewController.m
+//  IITC-Mobile
+//
+//  Created by Hubert Zhang on 15/7/28.
+//  Copyright © 2015年 IITC. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
