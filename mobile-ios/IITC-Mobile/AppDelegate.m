40c54352e6838bd52fe4260833983c572c7e9f11
Hubertzhang <hubert.zyk@gmail.com>
2015-08-15 19:31:52 +0800

    load scripts when start

--- mobile-ios/IITC-Mobile/AppDelegate.m
+++ mobile-ios/IITC-Mobile/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ScriptsManager.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [[ScriptsManager sharedInstance] loadLocalFiles];
     return YES;
 }
 
