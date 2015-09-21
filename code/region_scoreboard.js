ba8cb058ea56a13dac4eacdee19c558d57d7bb30
mcben <viertelvor12@gmx.net>
2015-09-21 16:36:02 +0200

    scoreboard: extracted HistoryChart

--- code/region_scoreboard.js
+++ code/region_scoreboard.js
@@ -27,8 +27,8 @@ var RegionScoreboard = (function () {
           return Math.max(this.getAvgScore(TEAM_ENL), this.getAvgScore(TEAM_RES), 1);
       }
       
-      this.getScoreMax = function() {
-          var max = 0;
+      this.getScoreMax = function(min_value) {
+          var max = min_value || 0;
           for (var i=1; i<this.checkpoints.length; i++) {
               var cp = this.checkpoints[i];
               max = Math.max(max,cp[0],cp[1]);
@@ -74,7 +74,7 @@ var RegionScoreboard = (function () {
     mainDialog.html('<div class="cellscore">'
            +'<b>Region scores for '+regionScore.regionName+'</b>'
            +'<div><table>'+createResults()+'</table>'
-           +createHistoryChart( logscale)+'</div>'
+           +RegionScoreboard.HistoryChart.create(regionScore, logscale)+'</div>'
            +'<b>Checkpoint overview</b>'
            +'<div>'+createHistoryTable()+'</div>'
            +'<b>Top agents</b>'
@@ -104,120 +104,6 @@ var RegionScoreboard = (function () {
     });
   }
    
-
-  function createHistoryChartPath(svgresult, scaleFct) {
-    var teamPaths = [[],[]];
-
-    for (var i=0; i<regionScore.checkpoints.length; i++) {
-      var x=i*10+40;
-        
-      if (regionScore.checkpoints[i] !== undefined) {
-          
-        // paths
-        if (i>0 && regionScore.checkpoints[i-1] !== undefined) {
-          for (var t=0; t<2; t++) {
-            teamPaths[t].push('M'+(x-10)+','+scaleFct(regionScore.checkpoints[i-1][t])+' L'+x+','+scaleFct(regionScore.checkpoints[i][t]));
-          }
-        }
-        // markers
-        svgresult.push('<g title="test" class="checkpoint" data-cp="'+i+'" data-enl="'+regionScore.checkpoints[i][0]+'" data-res="'+regionScore.checkpoints[i][1]+'">');
-        svgresult.push('<rect x="'+(i*10+35)+'" y="10" width="10" height="100" fill="black" fill-opacity="0" />');
-        for (var t=0; t<2; t++) {
-          var col = t==0 ? COLORS[TEAM_ENL] : COLORS[TEAM_RES];
-          svgresult.push('<circle cx="'+x+'" cy="'+scaleFct(regionScore.checkpoints[i][t])+'" r="3" stroke-width="1" stroke="'+col+'" fill="'+col+'" fill-opacity="0.5" />');
-        }
-        svgresult.push('</g>');
-      }
-    }
-    
-    return teamPaths;
-   }
-   
-  function createHistoryChartTicks(svgresult, scaleFct, logscale,max) {
-      // graph tickmarks - horizontal
-      var ticks = [];
-      for (var i=5; i<=35; i+=5) {
-          var x=i*10+40;
-          ticks.push('M'+x+',10 L'+x+',110');
-          svgresult.push('<text x="'+x+'" y="125" font-size="12" font-family="Roboto, Helvetica, sans-serif" text-anchor="middle" fill="#fff">'+i+'</text>');
-      }
-
-      // vertical
-      // first we calculate the power of 10 that is smaller than the max limit
-      var vtickStep = Math.pow(10,Math.floor(Math.log10(max)));
-      var vticks = [];
-      if(logscale) {
-          for(var i=0;i<4;i++) {
-          vticks.push(vtickStep);
-          vtickStep /= 10;
-          }
-      } else {
-          // this could be between 1 and 10 grid lines - so we adjust to give nicer spacings
-          if (vtickStep < (max/5)) {
-              vtickStep *= 2;
-          } else if (vtickStep > (max/2)) {
-              vtickStep /= 2;
-          }
-          for (var i=vtickStep; i<=max; i+=vtickStep) {
-          vticks.push(i);
-          }
-      }
-      vticks.forEach(function(i) {
-          var y = scaleFct(i);
-
-          ticks.push('M40,'+y+' L390,'+y);
-
-          var istr = i>=1000000000 ? i/1000000000+'B' : i>=1000000 ? i/1000000+'M' : i>=1000 ? i/1000+'k' : i;
-          svgresult.push('<text x="35" y="'+y+'" font-size="12" font-family="Roboto, Helvetica, sans-serif" text-anchor="end" fill="#fff">'+istr+'</text>');
-      });
-
-      return ('<path d="'+ticks.join(' ')+'" stroke="#fff" opacity="0.3" />');
-   }
-
-   
-  function createHistoryChart(logscale) {
-    // svg area 400x130. graph area 350x100, offset to 40,10
-
-    var max = Math.max(regionScore.getScoreMax(),10); //NOTE: ensure a min of 10 for the graph
-    max *= 1.09;      // scale up maximum a little, so graph isn't squashed right against upper edge
-
-    if(!Math.log10)
-      Math.log10 = function(x) { return Math.log(x) / Math.LN10; };
-      
-    // 0 cannot be displayed on a log scale, so we set the minimum to 0.001 and divide by lg(0.001)=-3
-    var scaleFct = logscale
-      ? function(y) { return  10 - Math.log10(Math.max(0.001,y/max)) / 3 * 100; }
-      : function(y) { return 110-y/max*100; };
-
-      
-      var otherSvg = [];
-      var teamPaths = createHistoryChartPath(otherSvg, scaleFct);
-
-      var ticks_path = createHistoryChartTicks(otherSvg, scaleFct, logscale,max);
-      
-      for (var t=0; t<2; t++) {
-          var col = t==0 ? COLORS[TEAM_ENL] : COLORS[TEAM_RES];
-          if (teamPaths[t].length > 0) {
-              ticks_path += '<path d="'+teamPaths[t].join(' ')+'" stroke="'+col+'" />';
-          }
-
-          var y = scaleFct(regionScore.gameScore[t]);
-          ticks_path += '<path d="M40,'+y+' L390,'+y+'" stroke="'+col+'" stroke-dasharray="3,2" opacity="0.8" />';
-      }
-
-      var svg = '<div><svg width="400" height="130">'
-             +'<rect x="0" y="0" width="400" height="130" stroke="#FFCE00" fill="#08304E" />'
-             +'<path d="M40,110 L40,10 M40,110 L390,110" stroke="#fff" />'
-             + ticks_path
-             +otherSvg.join('')
-             +'<foreignObject height="18" width="45" y="111" x="0" class="node"><label title="Logarithmic scale">'
-             +'<input type="checkbox" class="logscale" style="height:auto;padding:0;vertical-align:middle"'+(logscale?' checked':'')+'/>'
-             +'log</label></foreignObject>'
-             +'</svg></div>';
-
-      return svg;
-   }
-
   function createHistoryTable() {
     var history = regionScore.checkpoints;
     var table = '<table class="checkpoint_table"><thead><tr><th>Checkpoint</th><th>Enlightened</th><th>Resistance</th></tr></thead>';
@@ -260,7 +146,7 @@ var RegionScoreboard = (function () {
           var barSize = Math.round(regionScore.getAvgScore(faction)/maxAverage*200);
           result += '<tr><th class="'+teamClass+'">'+team+'</th>'
                   + '<td class="'+teamClass+'">'+digits(regionScore.getAvgScore(faction))+'</td>'
-                  +'<td><div style="background:'+teamCol+'; width: '+barSize+'px; height: 1.3ex; border: 2px outset '+teamCol+'"> </td></tr>';
+                  + '<td><div style="background:'+teamCol+'; width: '+barSize+'px; height: 1.3ex; border: 2px outset '+teamCol+'; margin-top: 2px"> </td></tr>';
       }
 
       return result;
@@ -273,3 +159,166 @@ var RegionScoreboard = (function () {
   
 }());
 
+
+RegionScoreboard.HistoryChart = (function () {
+  var regionScore;
+  var scaleFct;
+  var logscale;
+  var svgObjects;
+  
+  function create(_regionScore,logscale) {
+    regionScore = _regionScore;
+
+    var max = regionScore.getScoreMax(10); //NOTE: ensure a min of 10 for the graph
+    max *= 1.09;      // scale up maximum a little, so graph isn't squashed right against upper edge
+    setScaleType(max,logscale)
+      
+    svgObjects = [];
+
+    // svg area 400x130. graph area 350x100, offset to 40,10
+    var svg= '<div><svg width="400" height="130">'
+           + svgBackground()
+           + svgAxis(max)
+           + svgAveragePath()
+           + svgFactionPath()
+           + svgObjects.join('')
+           + '<foreignObject height="18" width="45" y="111" x="0" class="node"><label title="Logarithmic scale">'
+           +  '<input type="checkbox" class="logscale" style="height:auto;padding:0;vertical-align:middle"'+(logscale?' checked':'')+'/>'
+           +  'log</label></foreignObject>'
+           + '</svg></div>';
+
+    return svg;
+   }
+  
+  function svgFactionPath() {
+    var teamPaths = [[],[]];
+
+    for (var i=0; i<regionScore.checkpoints.length; i++) {
+      var x=i*10+40;
+        
+      if (regionScore.checkpoints[i] !== undefined) {
+          
+        // paths
+        if (i>0 && regionScore.checkpoints[i-1] !== undefined) {
+          for (var t=0; t<2; t++) {
+            teamPaths[t].push('M'+(x-10)+','+scaleFct(regionScore.checkpoints[i-1][t])+' L'+x+','+scaleFct(regionScore.checkpoints[i][t]));
+          }
+        }
+        // markers
+        svgObjects.push('<g title="test" class="checkpoint" data-cp="'+i+'" data-enl="'+regionScore.checkpoints[i][0]+'" data-res="'+regionScore.checkpoints[i][1]+'">');
+        svgObjects.push('<rect x="'+(i*10+35)+'" y="10" width="10" height="100" fill="black" fill-opacity="0" />');
+        for (var t=0; t<2; t++) {
+          var col = getFactionColor(t);
+          svgObjects.push('<circle cx="'+x+'" cy="'+scaleFct(regionScore.checkpoints[i][t])+'" r="3" stroke-width="1" stroke="'+col+'" fill="'+col+'" fill-opacity="0.5" />');
+        }
+        svgObjects.push('</g>');
+      }
+    }
+    
+    var svgPath='';
+    for (var t=0; t<2; t++) {
+        var col = getFactionColor(t);
+        if (teamPaths[t].length > 0) {
+            svgPath += '<path d="'+teamPaths[t].join(' ')+'" stroke="'+col+'" />';
+        }
+    }
+    
+    return svgPath;
+  }
+  
+   
+  function svgBackground() {
+    return '<rect x="0" y="0" width="400" height="130" stroke="#FFCE00" fill="#08304E" />';
+  }
+
+  function svgAxis(max) {
+    return '<path d="M40,110 L40,10 M40,110 L390,110" stroke="#fff" />'
+           + createTicks(max);
+  }
+
+  function createTicks(max) {
+      var ticks = createTicksHorz();
+
+      function addVTick(i) {
+          var y = scaleFct(i);
+
+          ticks.push('M40,'+y+' L390,'+y);
+        
+          svgObjects.push('<text x="35" y="'+y+'" font-size="12" font-family="Roboto, Helvetica, sans-serif" text-anchor="end" fill="#fff">'+formatNumber(i)+'</text>');
+      }
+    
+      // vertical
+      // first we calculate the power of 10 that is smaller than the max limit
+      var vtickStep = Math.pow(10,Math.floor(Math.log10(max)));
+      if(logscale) {
+          for(var i=0;i<4;i++) {
+            
+            addVTick(vtickStep);
+            vtickStep /= 10;
+          }
+      } else {
+          // this could be between 1 and 10 grid lines - so we adjust to give nicer spacings
+          if (vtickStep < (max/5)) {
+              vtickStep *= 2;
+          } else if (vtickStep > (max/2)) {
+              vtickStep /= 2;
+          }
+          
+          for (var i=vtickStep; i<=max; i+=vtickStep) {
+            addVTick(i);
+          }
+      }
+      
+
+      return ('<path d="'+ticks.join(' ')+'" stroke="#fff" opacity="0.3" />');
+  }   
+
+  function createTicksHorz() {
+    var ticks = [];
+    for (var i=5; i<=35; i+=5) {
+      var x=i*10+40;
+      ticks.push('M'+x+',10 L'+x+',110');
+      svgObjects.push('<text x="'+x+'" y="125" font-size="12" font-family="Roboto, Helvetica, sans-serif" text-anchor="middle" fill="#fff">'+i+'</text>');
+    }
+    
+    return ticks;
+  }
+   
+  function svgAveragePath() {
+    var path='';
+    for (var t=0; t<2; t++) {
+      var col = getFactionColor(t);
+      var y = scaleFct(regionScore.gameScore[t]);
+      path += '<path d="M40,'+y+' L390,'+y+'" stroke="'+col+'" stroke-dasharray="3,2" opacity="0.8" />';
+    }
+    
+    return path;
+  }
+  
+  function setScaleType(max,useLogScale) {
+    
+    logscale = useLogScale;
+    if (useLogScale) {
+      if(!Math.log10)
+        Math.log10 = function(x) { return Math.log(x) / Math.LN10; };
+        
+      // 0 cannot be displayed on a log scale, so we set the minimum to 0.001 and divide by lg(0.001)=-3
+      scaleFct = function(y) { return  10 - Math.log10(Math.max(0.001,y/max)) / 3 * 100; }
+    } else {
+      scaleFct = function(y) { return 110-y/max*100; };
+    }
+  }
+
+  function getFactionColor(t) {
+    return t==0 ? COLORS[TEAM_ENL] : COLORS[TEAM_RES];;
+  }
+
+  function formatNumber(num) {
+    return i>=1000000000 ? i/1000000000+'B' : i>=1000000 ? i/1000000+'M' : i>=1000 ? i/1000+'k' : i;
+  }
+  
+  return {
+    create
+  };
+
+}());
