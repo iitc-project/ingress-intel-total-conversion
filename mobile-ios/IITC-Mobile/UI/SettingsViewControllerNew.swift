fd28909da4f1b989b6e3b54b43c053be673f98f2
Hubertzhang <hubert.zyk@gmail.com>
2015-09-28 00:07:25 +0800

    add progress hud for updating

--- mobile-ios/IITC-Mobile/UI/SettingsViewControllerNew.swift
+++ mobile-ios/IITC-Mobile/UI/SettingsViewControllerNew.swift
@@ -11,8 +11,10 @@ import RxSwift
 import RxCocoa
 import RxBlocking
 import InAppSettingsKit
+import MBProgressHUD
 
 @objc class SettingsViewControllerNew: IASKAppSettingsViewController,IASKSettingsDelegate {
+
     override func viewDidLoad() {
         self.delegate = self
     }
@@ -22,12 +24,21 @@ import InAppSettingsKit
             let vc = self.navigationController!.storyboard!.instantiateViewControllerWithIdentifier("pluginsViewController")
             self.navigationController!.pushViewController(vc, animated:true)
         } else if (specifier.key() == "pref_update") {
-            
-            ScriptsManagerNew.updateFiles(ScriptsManager.sharedInstance().getAllScriptsPath()).subscribeOn(SerialDispatchQueueScheduler.init(internalSerialQueueName: "com.cradle.IITC-Mobile.network")).observeOn(MainScheduler.sharedInstance).subscribe(next: { (result) -> Void in
+            let hud = MBProgressHUD.showHUDAddedTo(self.navigationController!.view, animated:true);
+            hud.mode = MBProgressHUDMode.AnnularDeterminate;
+            hud.labelText = "Updating...";
+            var finished = 0
+            let scriptsPath = ScriptsManager.sharedInstance().getAllScriptsPath()
+            ScriptsManagerNew.updateFiles(scriptsPath).subscribeOn(SerialDispatchQueueScheduler.init(internalSerialQueueName: "com.cradle.IITC-Mobile.network")).observeOn(MainScheduler.sharedInstance).subscribe(next: { (result) -> Void in
                     print("1")
+                finished = finished + 1
+                hud.progress = Float(finished) / Float(scriptsPath.count)
                 }, error: { (e) -> Void in
                     print(e)
                 }, completed: { () -> Void in
+                    hud.labelText="Scanning..."
+                    ScriptsManager.sharedInstance().loadLocalFiles()
+                    hud.hide(true)
                     print("complete")
                 }, disposed: { () -> Void in
                     print("??")
