7c8c81b2273f929e01830566d517099aba10a07a
Tsuda Kageyu <tsuda.kageyu@gmail.com>
2016-04-28 15:16:46 +0900

    Clean up the code to stick to the coding style

--- plugins/basemap-gsi-japan.user.js
+++ plugins/basemap-gsi-japan.user.js
@@ -23,32 +23,30 @@
 // PLUGIN START ////////////////////////////////////////////////////////
 
 
-/*
-Map data © 国土地理院 (The Geospatial Information Authority of Japan)
-
-The bathymetric contours are derived from those contained within the GEBCO 
-Digital Atlas, published by the BODC on behalf of IOC and IHO (2003) 
-(http://www.gebco.net)
-
-海上保安庁許可第２２２５１０号（水路業務法第２５条に基づく類似刊行物）
-
-GSI's terms of use: http://www.gsi.go.jp/ENGLISH/page_e30286.html
-
-> The Terms of Use are compatible with the Creative Commons Attribution 
-> License 4.0 (hereinafter referred to as the CC License). This means that 
-> Content based on the Terms of Use may be used under the CC License in 
-> lieu of the Terms of Use.
-*/
+// Map data © 国土地理院 (The Geospatial Information Authority of Japan)
+//
+// The bathymetric contours are derived from those contained within the GEBCO
+// Digital Atlas, published by the BODC on behalf of IOC and IHO (2003)
+// (http://www.gebco.net)
+//
+// 海上保安庁許可第２２２５１０号（水路業務法第２５条に基づく類似刊行物）
+//
+// GSI's terms of use: http://www.gsi.go.jp/ENGLISH/page_e30286.html
+//
+// > The Terms of Use are compatible with the Creative Commons Attribution
+// > License 4.0 (hereinafter referred to as the CC License). This means that
+// > Content based on the Terms of Use may be used under the CC License in
+// > lieu of the Terms of Use.
 
 // use own namespace for plugin
-    
+
 window.plugin.mapTileGsiJapan = {
   addLayer: function() {
 
     // Register the GSI map tiles as a layer.
 
     var gsiLayer = new L.TileLayer(
-      'http://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', 
+      'http://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png',
       {
         attribution:   'Map data © <a href="http://www.gsi.go.jp/ENGLISH/index.html">国土地理院</a>',
         minZoom:       5,
