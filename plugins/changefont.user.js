eb89198c3fc1a3779537a626d49ac5adc776580e
umer936 <umer936@gmail.com>
2016-08-29 19:18:12 -0500

    Add font changer for issue 1090

---
+++ plugins/changefont.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @id             iitc-plugin-fontchanger@umer936
+// @name           IITC plugin: Changes the font to see agent names easier 
+// @category       Tweaks
+// @version        0.1.1.@@DATETIMEVERSION@@
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      @@UPDATEURL@@
+// @downloadURL    @@DOWNLOADURL@@
+// @description    [@@BUILDNAME@@-@@BUILDDATE@@] Changes the font to make it easier to differentiate I and l.
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @include        https://www.ingress.com/mission/*
+// @include        http://www.ingress.com/mission/*
+// @match          https://www.ingress.com/mission/*
+// @match          http://www.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
+
+@@PLUGINSTART@@
+
+// PLUGIN START ////////////////////////////////////////////////////////
+
+$(".nickname").css("font-family","Verdana");undefined;
+
+// PLUGIN END //////////////////////////////////////////////////////////
+
+@@PLUGINEND@@
