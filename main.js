82471ab7bc4056db3fd0dc76511ca25999801a8e
Norman <nhamer@users.noreply.github.com>
2017-10-03 10:17:41 -0700

    fix earth radius to same value used in leaflet and S2

--- main.js
+++ main.js
@@ -184,7 +184,7 @@ window.TEAM_TO_CSS = ['none', 'res', 'enl'];
 
 window.SLOT_TO_LAT = [0, Math.sqrt(2)/2, 1, Math.sqrt(2)/2, 0, -Math.sqrt(2)/2, -1, -Math.sqrt(2)/2];
 window.SLOT_TO_LNG = [1, Math.sqrt(2)/2, 0, -Math.sqrt(2)/2, -1, -Math.sqrt(2)/2, 0, Math.sqrt(2)/2];
-window.EARTH_RADIUS=6378137;
+window.EARTH_RADIUS=6367000;
 window.DEG2RAD = Math.PI / 180;
 
 // STORAGE ///////////////////////////////////////////////////////////
